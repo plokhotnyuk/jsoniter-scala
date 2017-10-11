d060e429527e7e52d9501adb8621d48ba7c49f9a
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2017-10-11 16:21:59 +0200

    Clean up of custom Play JSON formats (more efficient mapping to HashMap)

--- benchmark/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/CustomPlayJsonFormats.scala
+++ benchmark/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/CustomPlayJsonFormats.scala
@@ -2,11 +2,12 @@ package com.github.plokhotnyuk.jsoniter_scala
 
 import play.api.libs.json._
 
+import scala.collection.breakOut
 import scala.collection.immutable.HashMap
 
 object CustomPlayJsonFormats {
   implicit val hmStrFormat: OFormat[HashMap[String, Double]] = OFormat[HashMap[String, Double]] (
-    Reads(js => JsSuccess(HashMap(js.as[Map[String, Double]].toSeq: _*))),
+    Reads(js => JsSuccess[HashMap[String, Double]](js.as[Map[String, Double]].map(identity)(breakOut))),
     OWrites[HashMap[String, Double]](m => Json.toJsObject(m.toMap)))
 
   implicit val mapLongFormat: OFormat[Map[Long, Double]] = OFormat(
@@ -14,7 +15,7 @@ object CustomPlayJsonFormats {
     OWrites[Map[Long, Double]](m => Json.toJsObject(m.map { case (k, v) => (k.toString, v) })))
 
   implicit val hmLongFormat: OFormat[HashMap[Long, Double]] = OFormat(
-    Reads(js => JsSuccess(HashMap(js.as[Map[Long, Double]].toSeq: _*))),
+    Reads(js => JsSuccess[HashMap[Long, Double]](js.as[Map[Long, Double]].map(identity)(breakOut))),
     OWrites[HashMap[Long, Double]](m => Json.toJsObject(m.map { case (k, v) => (k.toString, v) })))
 
   implicit val hmIntFormat: OFormat[Map[Int, HashMap[Long, Double]]] = OFormat[Map[Int, HashMap[Long, Double]]](
