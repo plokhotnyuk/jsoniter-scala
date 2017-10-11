33c19cce6cdd6bc5bcd17f26e6a3c530af2d046d
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2017-10-11 16:13:00 +0200

    Clean up of custom Play JSON formats (remove implicit conversions & redundant transformations of collections)

--- benchmark/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/CustomPlayJsonFormats.scala
+++ benchmark/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/CustomPlayJsonFormats.scala
@@ -5,46 +5,19 @@ import play.api.libs.json._
 import scala.collection.immutable.HashMap
 
 object CustomPlayJsonFormats {
-
   implicit val hmStrFormat: OFormat[HashMap[String, Double]] = OFormat[HashMap[String, Double]] (
-    Reads { js =>
-      js.asOpt[Map[String, Double]]
-        .fold[JsResult[HashMap[String, Double]]](JsError(s"Cannot parse Json as Map $js"))(m =>
-        JsSuccess(HashMap(m.toSeq: _*))
-      )
-    },
-    OWrites[HashMap[String, Double]] { v =>
-      Json.toJsObject(Map(v.toSeq: _*))
-    }
-  )
+    Reads(js => JsSuccess(HashMap(js.as[Map[String, Double]].toSeq: _*))),
+    OWrites[HashMap[String, Double]](m => Json.toJsObject(m.toMap)))
 
   implicit val mapLongFormat: OFormat[Map[Long, Double]] = OFormat(
-    Reads[Map[Long, Double]] {js =>
-      JsSuccess(js.as[Map[String, Double]].map{case (k, v) => k.toLong -> v})
-    },
-    OWrites[Map[Long, Double]] {map =>
-      Json.toJsObject(map.map{case (k, value) => (k.toString, value)})
-    }
-  )
+    Reads(js => JsSuccess(js.as[Map[String, Double]].map { case (k, v) => (java.lang.Long.parseLong(k), v) })),
+    OWrites[Map[Long, Double]](m => Json.toJsObject(m.map { case (k, v) => (k.toString, v) })))
 
   implicit val hmLongFormat: OFormat[HashMap[Long, Double]] = OFormat(
-    Reads { js =>
-      js.asOpt[Map[Long, Double]]
-        .fold[JsResult[HashMap[Long, Double]]](JsError(s"Cannot parse Json as Map: $js"))(m =>
-        JsSuccess(HashMap(m.toSeq: _*))
-      )
-    },
-    OWrites[HashMap[Long, Double]] { v =>
-      Json.toJsObject(v.map{case (k, value) => (k.toString, value)})
-    }
-  )
+    Reads(js => JsSuccess(HashMap(js.as[Map[Long, Double]].toSeq: _*))),
+    OWrites[HashMap[Long, Double]](m => Json.toJsObject(m.map { case (k, v) => (k.toString, v) })))
 
   implicit val hmIntFormat: OFormat[Map[Int, HashMap[Long, Double]]] = OFormat[Map[Int, HashMap[Long, Double]]](
-    Reads { js =>
-      JsSuccess(js.as[Map[String, HashMap[Long, Double]]].map{case (k, v) => k.toInt -> v})
-    },
-    OWrites[Map[Int, HashMap[Long, Double]]] { v =>
-      Json.toJsObject(v.map{case (k, value) => k.toString -> Map(value.toSeq: _*)})
-    }
-  )
+    Reads(js => JsSuccess(js.as[Map[String, HashMap[Long, Double]]].map { case (k, v) => (java.lang.Integer.parseInt(k), v) })),
+    OWrites[Map[Int, HashMap[Long, Double]]](m => Json.toJsObject(m.map { case (k, v) => (k.toString, v.toMap) })))
 }
