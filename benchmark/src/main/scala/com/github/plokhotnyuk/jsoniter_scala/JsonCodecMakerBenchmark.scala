03fe0023acf514f6af75fdb03ae955811f06498f
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2017-11-17 15:34:17 +0100

    Move preferred sizes of buffers to reader & writer configs

--- benchmark/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/JsonCodecMakerBenchmark.scala
+++ benchmark/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/JsonCodecMakerBenchmark.scala
@@ -464,134 +464,134 @@ object GoogleMapsAPI {
 }
 
 object TwitterAPI {
-case class Entities(
-  hashtags: Seq[String],
-  symbols: Seq[String],
-  user_mentions: Seq[UserMentions],
-  urls: Seq[Urls])
+  case class Entities(
+    hashtags: Seq[String],
+    symbols: Seq[String],
+    user_mentions: Seq[UserMentions],
+    urls: Seq[Urls])
 
-case class Entities1(
-  url: Url,
-  description: Url)
+  case class UserEntities(
+    url: Url,
+    description: Url)
 
-case class RetweetedStatus(
-  created_at: String,
-  id: Long,
-  id_str: String,
-  text: String,
-  truncated: Boolean,
-  entities: Entities,
-  source: String,
-  in_reply_to_status_id: Option[String],
-  in_reply_to_status_id_str: Option[String],
-  in_reply_to_user_id: Option[String],
-  in_reply_to_user_id_str: Option[String],
-  in_reply_to_screen_name: Option[String],
-  user: User,
-  geo: Option[String],
-  coordinates: Option[String],
-  place: Option[String],
-  contributors: Option[String],
-  is_quote_status: Boolean,
-  retweet_count: Int,
-  favorite_count: Int,
-  favorited: Boolean,
-  retweeted: Boolean,
-  possibly_sensitive: Option[Boolean],
-  lang: String)
+  case class RetweetedStatus(
+    created_at: String,
+    id: Long,
+    id_str: String,
+    text: String,
+    truncated: Boolean,
+    entities: Entities,
+    source: String,
+    in_reply_to_status_id: Option[String],
+    in_reply_to_status_id_str: Option[String],
+    in_reply_to_user_id: Option[String],
+    in_reply_to_user_id_str: Option[String],
+    in_reply_to_screen_name: Option[String],
+    user: User,
+    geo: Option[String],
+    coordinates: Option[String],
+    place: Option[String],
+    contributors: Option[String],
+    is_quote_status: Boolean,
+    retweet_count: Int,
+    favorite_count: Int,
+    favorited: Boolean,
+    retweeted: Boolean,
+    possibly_sensitive: Option[Boolean],
+    lang: String)
 
-case class Tweet(
-  created_at: String,
-  id: Long,
-  id_str: String,
-  text: String,
-  truncated: Boolean,
-  entities: Entities,
-  source: String,
-  in_reply_to_status_id: Option[String],
-  in_reply_to_status_id_str: Option[String],
-  in_reply_to_user_id: Option[String],
-  in_reply_to_user_id_str: Option[String],
-  in_reply_to_screen_name: Option[String],
-  user: User,
-  geo: Option[String],
-  coordinates: Option[String],
-  place: Option[String],
-  contributors: Option[String],
-  retweeted_status: RetweetedStatus,
-  is_quote_status: Boolean,
-  retweet_count: Int,
-  favorite_count: Int,
-  favorited: Boolean,
-  retweeted: Boolean,
-  possibly_sensitive: Option[Boolean],
-  lang: String)
+  case class Tweet(
+    created_at: String,
+    id: Long,
+    id_str: String,
+    text: String,
+    truncated: Boolean,
+    entities: Entities,
+    source: String,
+    in_reply_to_status_id: Option[String],
+    in_reply_to_status_id_str: Option[String],
+    in_reply_to_user_id: Option[String],
+    in_reply_to_user_id_str: Option[String],
+    in_reply_to_screen_name: Option[String],
+    user: User,
+    geo: Option[String],
+    coordinates: Option[String],
+    place: Option[String],
+    contributors: Option[String],
+    retweeted_status: RetweetedStatus,
+    is_quote_status: Boolean,
+    retweet_count: Int,
+    favorite_count: Int,
+    favorited: Boolean,
+    retweeted: Boolean,
+    possibly_sensitive: Option[Boolean],
+    lang: String)
 
-case class Url(urls: Seq[Urls])
+  case class Url(urls: Seq[Urls])
 
-case class Urls(
-  url: String,
-  expanded_url: String,
-  display_url: String,
-  indices: Seq[Int])
+  case class Urls(
+    url: String,
+    expanded_url: String,
+    display_url: String,
+    indices: Seq[Int])
 
-case class User(
-  id: Long,
-  id_str: String,
-  name: String,
-  screen_name: String,
-  location: String,
-  description: String,
-  url: String,
-  entities: Entities1,
-  `protected`: Boolean,
-  followers_count: Int,
-  friends_count: Int,
-  listed_count: Int,
-  created_at: String,
-  favourites_count: Int,
-  utc_offset: Int,
-  time_zone: String,
-  geo_enabled: Boolean,
-  verified: Boolean,
-  statuses_count: Int,
-  lang: String,
-  contributors_enabled: Boolean,
-  is_translator: Boolean,
-  is_translation_enabled: Boolean,
-  profile_background_color: String,
-  profile_background_image_url: String,
-  profile_background_image_url_https: String,
-  profile_background_tile: Boolean,
-  profile_image_url: String,
-  profile_image_url_https: String,
-  profile_banner_url: String,
-  profile_link_color: String,
-  profile_sidebar_border_color: String,
-  profile_sidebar_fill_color: String,
-  profile_text_color: String,
-  profile_use_background_image: Boolean,
-  has_extended_profile: Boolean,
-  default_profile: Boolean,
-  default_profile_image: Boolean,
-  following: Boolean,
-  follow_request_sent: Boolean,
-  notifications: Boolean,
-  translator_type: String)
+  case class User(
+    id: Long,
+    id_str: String,
+    name: String,
+    screen_name: String,
+    location: String,
+    description: String,
+    url: String,
+    entities: UserEntities,
+    `protected`: Boolean,
+    followers_count: Int,
+    friends_count: Int,
+    listed_count: Int,
+    created_at: String,
+    favourites_count: Int,
+    utc_offset: Int,
+    time_zone: String,
+    geo_enabled: Boolean,
+    verified: Boolean,
+    statuses_count: Int,
+    lang: String,
+    contributors_enabled: Boolean,
+    is_translator: Boolean,
+    is_translation_enabled: Boolean,
+    profile_background_color: String,
+    profile_background_image_url: String,
+    profile_background_image_url_https: String,
+    profile_background_tile: Boolean,
+    profile_image_url: String,
+    profile_image_url_https: String,
+    profile_banner_url: String,
+    profile_link_color: String,
+    profile_sidebar_border_color: String,
+    profile_sidebar_fill_color: String,
+    profile_text_color: String,
+    profile_use_background_image: Boolean,
+    has_extended_profile: Boolean,
+    default_profile: Boolean,
+    default_profile_image: Boolean,
+    following: Boolean,
+    follow_request_sent: Boolean,
+    notifications: Boolean,
+    translator_type: String)
 
-case class UserMentions(
-  screen_name: String,
-  name: String,
-  id: Long,
-  id_str: String,
-  indices: Seq[Int])
+  case class UserMentions(
+    screen_name: String,
+    name: String,
+    id: Long,
+    id_str: String,
+    indices: Seq[Int])
 
-/* FIXME: format doesn't compile
+  /* FIXME: format doesn't compile
   val format: OFormat[Seq[TwitterAPI.Tweet]] = Json.format[Seq[TwitterAPI.Tweet]]
-*/
-val codec: JsonCodec[Seq[Tweet]] = make[Seq[TwitterAPI.Tweet]](CodecMakerConfig())
-val json: Array[Byte] = Streamable.bytes(getClass.getResourceAsStream("twitter_api_response.json"))
-// TODO consider to make obj & compactJson explicitly defined instead of reading from file by a generated codec
-val obj: Seq[TwitterAPI.Tweet] = JsonReader.read(codec, json)
-val compactJson: Array[Byte] = JsonWriter.write(codec, obj)
+  */
+  val codec: JsonCodec[Seq[Tweet]] = make[Seq[TwitterAPI.Tweet]](CodecMakerConfig())
+  val json: Array[Byte] = Streamable.bytes(getClass.getResourceAsStream("twitter_api_response.json"))
+  // TODO consider to make obj & compactJson explicitly defined instead of reading from file by a generated codec
+  val obj: Seq[TwitterAPI.Tweet] = JsonReader.read(codec, json)
+  val compactJson: Array[Byte] = JsonWriter.write(codec, obj)
 }
