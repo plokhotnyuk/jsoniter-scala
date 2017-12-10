4d9a115d5747b304e4fa2ce58aba4aad46703d36
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2017-12-10 21:19:32 +0100

    Clean up benchmark code

--- benchmark/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/macros/JacksonSerDesers.scala
+++ benchmark/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/macros/JacksonSerDesers.scala
@@ -15,55 +15,6 @@ import scala.collection.immutable.BitSet
 import scala.collection.mutable
 
 object JacksonSerDesers {
-  class BitSetDeserializer extends StdDeserializer[BitSet](classOf[BitSet]) {
-    override def deserialize(p: JsonParser, ctxt: DeserializationContext): BitSet =
-      if (p.getCurrentToken != START_ARRAY) throw new ParseException(p, "expected '[' or null")
-      else if (p.nextToken() == END_ARRAY) getNullValue(ctxt)
-      else {
-        val s = BitSet.newBuilder
-        do {
-          s += p.getIntValue
-        } while (p.nextToken() != END_ARRAY)
-        s.result
-      }
-
-    override def getNullValue(ctxt: DeserializationContext): BitSet = BitSet.empty
-  }
-
-  class BitSetSerializer extends StdSerializer[BitSet](classOf[BitSet]) {
-    override def serialize(value: BitSet, gen: JsonGenerator, provider: SerializerProvider): Unit = {
-      gen.writeStartArray()
-      if (!isEmpty(provider, value)) value.foreach(gen.writeNumber)
-      gen.writeEndArray()
-    }
-
-    override def isEmpty(provider: SerializerProvider, value: BitSet): Boolean = value.isEmpty
-  }
-
-  class MutableBitSetDeserializer extends StdDeserializer[mutable.BitSet](classOf[mutable.BitSet]) {
-    override def deserialize(p: JsonParser, ctxt: DeserializationContext): mutable.BitSet =
-      if (p.getCurrentToken != START_ARRAY) throw new ParseException(p, "expected '[' or null")
-      else {
-        val s = getNullValue(ctxt)
-        while (p.nextToken() != END_ARRAY) {
-          s.add(p.getIntValue)
-        }
-        s
-      }
-
-    override def getNullValue(ctxt: DeserializationContext): mutable.BitSet = new mutable.BitSet
-  }
-
-  class MutableBitSetSerializer extends StdSerializer[mutable.BitSet](classOf[mutable.BitSet]) {
-    override def serialize(value: mutable.BitSet, gen: JsonGenerator, provider: SerializerProvider): Unit = {
-      gen.writeStartArray()
-      if (!isEmpty(provider, value)) value.foreach(gen.writeNumber)
-      gen.writeEndArray()
-    }
-
-    override def isEmpty(provider: SerializerProvider, value: mutable.BitSet): Boolean = value.isEmpty
-  }
-
   val jacksonMapper: ObjectMapper with ScalaObjectMapper = new ObjectMapper(new JsonFactory {
     disable(JsonFactory.Feature.INTERN_FIELD_NAMES)
   }) with ScalaObjectMapper {
@@ -78,4 +29,53 @@ object JacksonSerDesers {
     configure(JsonParser.Feature.ALLOW_UNQUOTED_CONTROL_CHARS, true)
     setSerializationInclusion(Include.NON_EMPTY)
   }
+}
+
+class BitSetDeserializer extends StdDeserializer[BitSet](classOf[BitSet]) {
+  override def deserialize(p: JsonParser, ctxt: DeserializationContext): BitSet =
+    if (p.getCurrentToken != START_ARRAY) throw new ParseException(p, "expected '[' or null")
+    else if (p.nextToken() == END_ARRAY) getNullValue(ctxt)
+    else {
+      val s = BitSet.newBuilder
+      do {
+        s += p.getIntValue
+      } while (p.nextToken() != END_ARRAY)
+      s.result
+    }
+
+  override def getNullValue(ctxt: DeserializationContext): BitSet = BitSet.empty
+}
+
+class BitSetSerializer extends StdSerializer[BitSet](classOf[BitSet]) {
+  override def serialize(value: BitSet, gen: JsonGenerator, provider: SerializerProvider): Unit = {
+    gen.writeStartArray()
+    if (!isEmpty(provider, value)) value.foreach(gen.writeNumber)
+    gen.writeEndArray()
+  }
+
+  override def isEmpty(provider: SerializerProvider, value: BitSet): Boolean = value.isEmpty
+}
+
+class MutableBitSetDeserializer extends StdDeserializer[mutable.BitSet](classOf[mutable.BitSet]) {
+  override def deserialize(p: JsonParser, ctxt: DeserializationContext): mutable.BitSet =
+    if (p.getCurrentToken != START_ARRAY) throw new ParseException(p, "expected '[' or null")
+    else {
+      val s = getNullValue(ctxt)
+      while (p.nextToken() != END_ARRAY) {
+        s.add(p.getIntValue)
+      }
+      s
+    }
+
+  override def getNullValue(ctxt: DeserializationContext): mutable.BitSet = new mutable.BitSet
+}
+
+class MutableBitSetSerializer extends StdSerializer[mutable.BitSet](classOf[mutable.BitSet]) {
+  override def serialize(value: mutable.BitSet, gen: JsonGenerator, provider: SerializerProvider): Unit = {
+    gen.writeStartArray()
+    if (!isEmpty(provider, value)) value.foreach(gen.writeNumber)
+    gen.writeEndArray()
+  }
+
+  override def isEmpty(provider: SerializerProvider, value: mutable.BitSet): Boolean = value.isEmpty
 }
