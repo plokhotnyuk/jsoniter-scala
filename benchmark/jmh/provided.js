dec199985f0e66c0560eb0421c85d990eb520994
Kostyantyn <kostyantyn@riskident.com>
2018-01-04 11:18:04 +0100

    visualised benchmark results

---
+++ benchmark/jmh/provided.js
@@ -0,0 +1,9 @@
+// provided.js can be overriden in order to have a predefined-report generated.
+
+
+var providedBenchmarks = []; // eslint-disable-line no-unused-vars
+// var providedBenchmarks = ['result']; // eslint-disable-line no-unused-vars
+
+var providedBenchmarkStore = { // eslint-disable-line no-unused-vars
+    result: [{}]
+};
