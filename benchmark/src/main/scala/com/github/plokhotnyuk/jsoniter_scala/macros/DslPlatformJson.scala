ed28e31ecfcc2920961e6691e825f6ab8ad4c8d6
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2018-04-01 23:49:23 +0200

    Add Dsl JSON Platform benchmarks

---
+++ benchmark/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/macros/DslPlatformJson.scala
@@ -0,0 +1,7 @@
+package com.github.plokhotnyuk.jsoniter_scala.macros
+
+import com.dslplatform.json.DslJson
+
+object DslPlatformJson {
+  val dslJson = new DslJson[Any]
+}
--- benchmark/src/test/scala/com/github/plokhotnyuk/jsoniter_scala/macros/AnyRefsBenchmarkSpec.scala
+++ benchmark/src/test/scala/com/github/plokhotnyuk/jsoniter_scala/macros/AnyRefsBenchmarkSpec.scala
@@ -6,13 +6,14 @@ class AnyRefsBenchmarkSpec extends BenchmarkSpecBase {
   "AnyRefsBenchmark" should {
     "deserialize properly" in {
       benchmark.readCirce() shouldBe benchmark.obj
+      benchmark.readDslJsonScala() shouldBe benchmark.obj
       benchmark.readJacksonScala() shouldBe benchmark.obj
       benchmark.readJsoniterScala() shouldBe benchmark.obj
       benchmark.readPlayJson() shouldBe benchmark.obj
     }
     "serialize properly" in {
       toString(benchmark.writeCirce()) shouldBe benchmark.jsonString
-      toString(benchmark.writeJacksonScala()) shouldBe benchmark.jsonString
+      toString(benchmark.writeDslJsonScala()) shouldBe benchmark.jsonString
       toString(benchmark.writeJsoniterScala()) shouldBe benchmark.jsonString
       toString(benchmark.preallocatedBuf, benchmark.writeJsoniterScalaPrealloc()) shouldBe benchmark.jsonString
       toString(benchmark.writePlayJson()) shouldBe benchmark.jsonString
