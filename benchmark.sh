c17777d1ca243fd4a63c8d625736b41c7d95dcb4
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2018-06-01 13:09:25 +0200

    Revert unwanted changes

--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#sbt -java-home /usr/lib/jvm/java-8-oracle -no-colors ++2.12.6 clean 'benchmark/jmh:run -p size=128 -jvm /usr/lib/jvm/graalvm-ee-1.0.0-rc1/bin/java -prof gc -rf json -rff graalvmee1.json .*Benchmark.*' >graalvmee1.txt
-#sbt -java-home /usr/lib/jvm/java-8-oracle -no-colors ++2.12.6 clean 'benchmark/jmh:run -p size=128 -jvm /usr/lib/jvm/graalvm-ce-1.0.0-rc1/bin/java -prof gc -rf json -rff graalvmce1.json .*Benchmark.*' >graalvmce1.txt
-#sbt -java-home /usr/lib/jvm/java-8-oracle -no-colors ++2.12.6 clean 'benchmark/jmh:run -p size=128 -jvm /usr/lib/jvm/java-8-oracle/bin/java -prof gc -rf json -rff jdk8.json .*Benchmark.*' >jdk8.txt
-#sbt -java-home /usr/lib/jvm/java-8-oracle -no-colors ++2.12.6 clean 'benchmark/jmh:run -p size=128 -jvm /usr/lib/jvm/jdk-10/bin/java -prof gc -rf json -rff jdk10.json .*Benchmark.*' >jdk10.txt
+sbt -java-home /usr/lib/jvm/java-8-oracle -no-colors ++2.12.6 clean 'benchmark/jmh:run -p size=128 -jvm /usr/lib/jvm/graalvm-ee-1.0.0-rc1/bin/java -prof gc -rf json -rff graalvmee1.json .*Benchmark.*' >graalvmee1.txt
+sbt -java-home /usr/lib/jvm/java-8-oracle -no-colors ++2.12.6 clean 'benchmark/jmh:run -p size=128 -jvm /usr/lib/jvm/graalvm-ce-1.0.0-rc1/bin/java -prof gc -rf json -rff graalvmce1.json .*Benchmark.*' >graalvmce1.txt
+sbt -java-home /usr/lib/jvm/java-8-oracle -no-colors ++2.12.6 clean 'benchmark/jmh:run -p size=128 -jvm /usr/lib/jvm/java-8-oracle/bin/java -prof gc -rf json -rff jdk8.json .*Benchmark.*' >jdk8.txt
+sbt -java-home /usr/lib/jvm/java-8-oracle -no-colors ++2.12.6 clean 'benchmark/jmh:run -p size=128 -jvm /usr/lib/jvm/jdk-10/bin/java -prof gc -rf json -rff jdk10.json .*Benchmark.*' >jdk10.txt
 sbt -java-home /usr/lib/jvm/java-8-oracle -no-colors ++2.12.6 clean 'benchmark/jmh:run -p size=128 -jvm /usr/lib/jvm/jdk-10/bin/java -jvmArgsAppend "-XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI -XX:+UseJVMCICompiler" -prof gc -rf json -rff jdk10graal.json .*Benchmark.*' >jdk10graal.txt
