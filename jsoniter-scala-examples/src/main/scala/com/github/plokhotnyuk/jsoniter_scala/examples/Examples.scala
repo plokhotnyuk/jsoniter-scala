bee12dd663fb662bc85643b8d7997fad710cded8
Constantin Gerstberger <constantin.gerstberger@xaasify.com>
2018-07-11 11:39:34 +0200

    add example

--- jsoniter-scala-examples/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/examples/Examples.scala
+++ jsoniter-scala-examples/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/examples/Examples.scala
@@ -2,17 +2,26 @@ package com.github.plokhotnyuk.jsoniter_scala.examples
 
 import com.github.plokhotnyuk.jsoniter_scala.macros._
 import com.github.plokhotnyuk.jsoniter_scala.core._
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.collection.NonEmpty
+import eu.timepit.refined.numeric.NonNegative
+import eu.timepit.refined._
 
-case class Device(id: Int, model: String)
+case class Device(id: Int Refined NonNegative, model: String Refined NonEmpty)
 
 case class User(name: String, devices: Seq[Device])
 
-object Examples {
+object Examples extends JsoniterRefined {
   def main(args: Array[String]): Unit = {
+
+    // comment out to see compile error
+    implicit val codecForIntNonNegative: JsonCodec[Refined[Int, NonNegative]] = intCodec[Refined, NonNegative]
+    implicit val codecForStringNonEmpty: JsonCodec[Refined[String, NonEmpty]] = stringCodec[Refined, NonEmpty]
+
     implicit val codec: JsonValueCodec[User] = JsonCodecMaker.make[User](CodecMakerConfig())
 
-    val user = readFromArray("""{"name":"John","devices":[{"id":1,"model":"HTC One X"}]}""".getBytes("UTF-8"))
-    val json = writeToArray(User(name = "John", devices = Seq(Device(id = 2, model = "iPhone X"))))
+    val user = readFromArray[User]("""{"name":"John","devices":[{"id":1,"model":"HTC One X"}]}""".getBytes("UTF-8"))
+    val json = writeToArray(User(name = "John", devices = Seq(Device(id = refineMV(2), model = refineMV("iPhone X")))))
 
     println(user)
     println(new String(json, "UTF-8"))
---
+++ jsoniter-scala-examples/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/examples/JsoniterRefined.scala
@@ -0,0 +1,130 @@
+package com.github.plokhotnyuk.jsoniter_scala.examples
+
+import com.github.plokhotnyuk.jsoniter_scala.core.{JsonCodec, JsonReader, JsonWriter}
+import eu.timepit.refined.api.{RefType, Validate}
+
+import scala.language.higherKinds
+import scala.reflect.runtime.universe.WeakTypeTag
+
+// TODO: how to deal with refined collections? implicit JsonCodec[T] seems not to be visible for primitive types ...
+trait JsoniterRefined {
+
+  // ===========================================================================================================================================================
+  // INT
+  // ===========================================================================================================================================================
+  implicit def intCodec[F[_, _], P](
+                                     implicit refType: RefType[F],
+                                     validate: Validate[Int, P],
+                                     typeTag: WeakTypeTag[F[Int, P]]
+                                   ): JsonCodec[F[Int, P]] = {
+
+    new JsonCodec[F[Int, P]] {
+
+      override def decodeKey(in: JsonReader): F[Int, P] = {
+        val unrefined = in.readKeyAsInt()
+
+        refType refine unrefined match {
+          case Left(because) =>
+            in decodeError s"Cannot convert '$unrefined' to ${typeTag.tpe.toString} because: $because"
+          case Right(refined) =>
+            refined
+        }
+      }
+
+      override def encodeKey(refined: F[Int, P], out: JsonWriter): Unit = {
+        val unrefined = refType unwrap refined
+        out writeKey unrefined
+      }
+
+      override def decodeValue(in: JsonReader,
+                               default: F[Int, P]): F[Int, P] = {
+        if (in isNextToken 'n') {
+          in.rollbackToken()
+          in.readNullOrError(default, "expected int value or null")
+        } else {
+          val unrefined = in.readInt()
+          refType refine unrefined match {
+            case Left(because) =>
+              in decodeError s"Cannot convert '$unrefined' to ${typeTag.tpe.toString} because: $because"
+            case Right(refined) =>
+              refined
+          }
+        }
+      }
+
+      override def encodeValue(refined: F[Int, P], out: JsonWriter): Unit = {
+        if (refined != null) {
+          val unrefined = refType unwrap refined
+          out writeVal unrefined
+        } else {
+          out.writeNull()
+        }
+
+      }
+
+      override def nullValue: F[Int, P] = null.asInstanceOf[F[Int, P]]
+    }
+
+  }
+
+  // ===========================================================================================================================================================
+  // STRING
+  // ===========================================================================================================================================================
+  implicit def stringCodec[F[_, _], P](
+                                        implicit refType: RefType[F],
+                                        validate: Validate[String, P],
+                                        typeTag: WeakTypeTag[F[String, P]]
+                                      ): JsonCodec[F[String, P]] = {
+
+    new JsonCodec[F[String, P]] {
+
+      override def decodeKey(in: JsonReader): F[String, P] = {
+        val unrefined = in.readKeyAsString()
+
+        refType refine unrefined match {
+          case Left(because) =>
+            in decodeError s"Cannot convert '$unrefined' to ${typeTag.tpe.toString} because: $because"
+          case Right(refined) =>
+            refined
+        }
+      }
+
+      override def encodeKey(refined: F[String, P], out: JsonWriter): Unit = {
+        val unrefined = refType unwrap refined
+        out writeKey unrefined
+      }
+
+      override def decodeValue(in: JsonReader,
+                               default: F[String, P]): F[String, P] = {
+        if (in isNextToken 'n') {
+          in.rollbackToken()
+          in.readNullOrError(default, "expected String value or null")
+        } else {
+          // TODO: default correct?
+          val unrefined = in.readString(refType unwrap default)
+          refType refine unrefined match {
+            case Left(because) =>
+              in decodeError s"Cannot convert '$unrefined' to ${typeTag.tpe.toString} because: $because"
+            case Right(refined) =>
+              refined
+          }
+        }
+      }
+
+      override def encodeValue(refined: F[String, P], out: JsonWriter): Unit = {
+        if (refined != null) {
+          val unrefined = refType unwrap refined
+          out writeVal unrefined
+        } else {
+          out.writeNull()
+        }
+
+      }
+
+      override def nullValue: F[String, P] = null.asInstanceOf[F[String, P]]
+    }
+
+  }
+
+
+}
