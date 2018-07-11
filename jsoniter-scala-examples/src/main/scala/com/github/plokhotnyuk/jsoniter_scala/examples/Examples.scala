b7e4588de8a37d0a9008be8cb7066af9b9bd86b8
Constantin Gerstberger <constantin.gerstberger@xaasify.com>
2018-07-11 19:00:01 +0200

    implement codec without type tag

--- jsoniter-scala-examples/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/examples/Examples.scala
+++ jsoniter-scala-examples/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/examples/Examples.scala
@@ -11,7 +11,7 @@ case class Device(id: Int Refined NonNegative, model: String Refined NonEmpty)
 
 case class User(name: String, devices: Seq[Device])
 
-object Examples extends JsoniterRefined {
+object Examples extends JsoniterRefinedNoTypeTag {
 
   def main(args: Array[String]): Unit = {
 
@@ -21,7 +21,7 @@ object Examples extends JsoniterRefined {
 
     implicit val codec: JsonValueCodec[User] = JsonCodecMaker.make[User](CodecMakerConfig())
 
-    val user = readFromArray[User]("""{"name":"John","devices":[{"id":1,"model":"HTC One X"}]}""".getBytes("UTF-8"))
+    val user = readFromArray[User]("""{"name":"John","devices":[{"id":-1,"model":"HTC One X"}]}""".getBytes("UTF-8"))
     val json = writeToArray(User(name = "John", devices = Seq(Device(id = refineMV(2), model = refineMV("iPhone X")))))
 
     println(user)
---
+++ jsoniter-scala-examples/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/examples/JsoniterRefinedNoTypeTag.scala
@@ -0,0 +1,123 @@
+package com.github.plokhotnyuk.jsoniter_scala.examples
+
+import com.github.plokhotnyuk.jsoniter_scala.core.{JsonCodec, JsonReader, JsonWriter}
+import eu.timepit.refined.api.{RefType, Validate}
+
+trait JsoniterRefinedNoTypeTag {
+
+  // ===========================================================================================================================================================
+  // INT
+  // ===========================================================================================================================================================
+  implicit def intCodec[F[_, _], P](
+                                     implicit refType: RefType[F],
+                                     validate: Validate[Int, P],
+                                   ): JsonCodec[F[Int, P]] = {
+
+    new JsonCodec[F[Int, P]] {
+
+      override def decodeKey(in: JsonReader): F[Int, P] = {
+        val unrefined = in.readKeyAsInt()
+
+        refType refine unrefined match {
+          case Left(because) =>
+            in decodeError s"Cannot refined unrefined Int '$unrefined' because: $because"
+          case Right(refined) =>
+            refined
+        }
+      }
+
+      override def encodeKey(refined: F[Int, P], out: JsonWriter): Unit = {
+        val unrefined = refType unwrap refined
+        out writeKey unrefined
+      }
+
+      override def decodeValue(in: JsonReader,
+                               default: F[Int, P]): F[Int, P] = {
+        if (in isNextToken 'n') {
+          in.readNullOrError(default, "expected Int value or null")
+        } else {
+          in.rollbackToken()
+          val unrefined = in.readInt()
+          refType refine unrefined match {
+            case Left(because) =>
+              in decodeError s"Cannot refined unrefined Int '$unrefined' because: $because"
+            case Right(refined) =>
+              refined
+          }
+        }
+      }
+
+      override def encodeValue(refined: F[Int, P], out: JsonWriter): Unit = {
+        if (refined != null) {
+          val unrefined = refType unwrap refined
+          out writeVal unrefined
+        } else {
+          out.writeNull()
+        }
+
+      }
+
+      override def nullValue: F[Int, P] = null.asInstanceOf[F[Int, P]]
+    }
+
+  }
+
+  // ===========================================================================================================================================================
+  // STRING
+  // ===========================================================================================================================================================
+  implicit def stringCodec[F[_, _], P](
+                                        implicit refType: RefType[F],
+                                        validate: Validate[String, P],
+                                      ): JsonCodec[F[String, P]] = {
+
+    new JsonCodec[F[String, P]] {
+
+      override def decodeKey(in: JsonReader): F[String, P] = {
+        val unrefined = in.readKeyAsString()
+
+        refType refine unrefined match {
+          case Left(because) =>
+            in decodeError s"Cannot refined unrefined String '$unrefined' because: $because"
+          case Right(refined) =>
+            refined
+        }
+      }
+
+      override def encodeKey(refined: F[String, P], out: JsonWriter): Unit = {
+        val unrefined = refType unwrap refined
+        out writeKey unrefined
+      }
+
+      override def decodeValue(in: JsonReader,
+                               default: F[String, P]): F[String, P] = {
+        if (in isNextToken 'n') {
+          in.readNullOrError(default, "expected String value or null")
+        } else {
+          in.rollbackToken()
+          // TODO: default correct?
+          val unrefined = in.readString(refType unwrap default)
+          refType refine unrefined match {
+            case Left(because) =>
+              in decodeError s"CCannot refined unrefined String '$unrefined' because: $because"
+            case Right(refined) =>
+              refined
+          }
+        }
+      }
+
+      override def encodeValue(refined: F[String, P], out: JsonWriter): Unit = {
+        if (refined != null) {
+          val unrefined = refType unwrap refined
+          out writeVal unrefined
+        } else {
+          out.writeNull()
+        }
+
+      }
+
+      override def nullValue: F[String, P] = null.asInstanceOf[F[String, P]]
+    }
+
+  }
+
+}
