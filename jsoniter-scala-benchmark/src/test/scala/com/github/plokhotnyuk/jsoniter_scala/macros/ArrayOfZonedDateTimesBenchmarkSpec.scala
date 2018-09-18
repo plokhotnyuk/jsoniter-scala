0d53faf5093b492867b550f2cec55ff0b5cc62de
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2018-09-18 13:51:28 +0200

    Reproduce the same DoS for Jackson-module-scala https://github.com/playframework/play-json/issues/180

--- jsoniter-scala-benchmark/src/test/scala/com/github/plokhotnyuk/jsoniter_scala/macros/ArrayOfZonedDateTimesBenchmarkSpec.scala
+++ jsoniter-scala-benchmark/src/test/scala/com/github/plokhotnyuk/jsoniter_scala/macros/ArrayOfZonedDateTimesBenchmarkSpec.scala
@@ -7,16 +7,12 @@ class ArrayOfZonedDateTimesBenchmarkSpec extends BenchmarkSpecBase {
   
   "ArrayOfZonedDateTimesBenchmark" should {
     "deserialize properly" in {
-/*
-      benchmark.readAVSystemGenCodec() shouldBe benchmark.obj
-      benchmark.readCirce() shouldBe benchmark.obj
-      benchmark.readJacksonScala() shouldBe benchmark.obj
-      benchmark.readJsoniterScala() shouldBe benchmark.obj
-*/
-      benchmark.readPlayJson() shouldBe benchmark.obj
-/*
-      benchmark.readUPickle() shouldBe benchmark.obj
-*/
+//      benchmark.readAVSystemGenCodec() shouldBe benchmark.obj
+//      benchmark.readCirce() shouldBe benchmark.obj
+            benchmark.readJacksonScala() shouldBe benchmark.obj
+//            benchmark.readJsoniterScala() shouldBe benchmark.obj
+ //     benchmark.readPlayJson() shouldBe benchmark.obj
+ //     benchmark.readUPickle() shouldBe benchmark.obj
     }
     "serialize properly" in {
 /*
