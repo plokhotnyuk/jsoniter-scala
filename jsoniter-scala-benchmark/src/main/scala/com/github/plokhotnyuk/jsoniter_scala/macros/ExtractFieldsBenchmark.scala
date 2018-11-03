db0f661bc7070576505e3dcc2a781c95748b01f9
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2018-11-03 07:32:00 +0100

    Set default payload for ignored values in ExtractFieldsBenchmark

--- jsoniter-scala-benchmark/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/macros/ExtractFieldsBenchmark.scala
+++ jsoniter-scala-benchmark/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/macros/ExtractFieldsBenchmark.scala
@@ -25,8 +25,8 @@ case class ExtractFields(s: String, i: Int)
 class ExtractFieldsBenchmark extends CommonParams {
   @Param(Array("1", "10", "100", "1000", "10000", "100000"/*, "1000000" FIXME: uncomment when patch for this issue will be released: https://github.com/playframework/play-json/issues/186 */))
   var size: Int = 10
-  @Param(Array("""null"""))
-  var value = """null"""
+  @Param(Array("""[0.0,false,null]"""))
+  var value = """[0.0,false,null]"""
   var obj: ExtractFields = ExtractFields("s", 1)
   var jsonString: String = _
   var jsonBytes: Array[Byte] = _
