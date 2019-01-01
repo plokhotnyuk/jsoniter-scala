918e605ea79a1e2aaf447bff7e4cf950f8870e43
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2019-01-01 14:10:14 +0100

    Code clean up

--- jsoniter-scala-benchmark/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/benchmark/CirceEncodersDecoders.scala
+++ jsoniter-scala-benchmark/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/benchmark/CirceEncodersDecoders.scala
@@ -47,11 +47,13 @@ object CirceEncodersDecoders {
   }
   implicit val bigIntEncoder: Encoder[BigInt] = Encoder.encodeJsonNumber
     .contramap(x => JsonNumber.fromDecimalStringUnsafe(new java.math.BigDecimal(x.bigInteger).toPlainString))
-  implicit val enumEncoder: Encoder[SuitEnum] = Encoder.enumEncoder(SuitEnum)
-  implicit val enumDecoder: Decoder[SuitEnum] = Decoder.enumDecoder(SuitEnum)
   implicit val suitEncoder: Encoder[Suit] = Encoder.encodeString.contramap(_.name)
   implicit val suitDecoder: Decoder[Suit] = Decoder.decodeString
     .emap(s => Try(Suit.valueOf(s)).fold[Either[String, Suit]](_ => Left("Suit"), Right.apply))
+  implicit val suitADTEncoder: Encoder[SuitADT] = deriveEnumerationEncoder
+  implicit val suitADTDecoder: Decoder[SuitADT] = deriveEnumerationDecoder
+  implicit val suitEnumEncoder: Encoder[SuitEnum] = Encoder.enumEncoder(SuitEnum)
+  implicit val suitEnumDecoder: Decoder[SuitEnum] = Decoder.enumDecoder(SuitEnum)
   implicit val geometryEncoder: Encoder[Geometry] = {
     implicit val pointEncoder: Encoder[Point] = deriveEncoder
     implicit val multiPointEncoder: Encoder[MultiPoint] = deriveEncoder
@@ -82,42 +84,4 @@ object CirceEncodersDecoders {
     implicit val featureCollectionDecoder: Decoder[FeatureCollection] = deriveDecoder
     deriveDecoder
   }
-  // Derivation for Enum ADTs borrowed from:
-  // https://stackoverflow.com/questions/37011894/circe-instances-for-encoding-decoding-sealed-trait-instances-of-arity-0
-  import shapeless._
-  import shapeless.labelled.{ FieldType, field }
-
-  trait IsEnum[C <: Coproduct] {
-    def to(c: C): String
-
-    def from(s: String): Option[C]
-  }
-
-  object IsEnum {
-    implicit val cnilIsEnum: IsEnum[CNil] = new IsEnum[CNil] {
-      def to(c: CNil): String = sys.error("Impossible")
-
-      def from(s: String): Option[CNil] = None
-    }
-
-    implicit def cconsIsEnum[K <: Symbol, H <: Product, T <: Coproduct](implicit witK: Witness.Aux[K],
-                                                                        witH: Witness.Aux[H],
-                                                                        tie: IsEnum[T]): IsEnum[FieldType[K, H] :+: T] =
-      new IsEnum[FieldType[K, H] :+: T] {
-        def to(c: FieldType[K, H] :+: T): String = c match {
-          case Inl(h) => witK.value.name
-          case Inr(t) => tie.to(t)
-        }
-
-        def from(s: String): Option[FieldType[K, H] :+: T] =
-          if (s == witK.value.name) Some(Inl(field[K](witH.value)))
-          else tie.from(s).map(Inr(_))
-      }
-  }
-
-  implicit def encodeEnum[A, C <: Coproduct](implicit gen: LabelledGeneric.Aux[A, C], rie: IsEnum[C]): Encoder[A] =
-    Encoder[String].contramap[A](a => rie.to(gen.to(a)))
-
-  implicit def decodeEnum[A, C <: Coproduct](implicit gen: LabelledGeneric.Aux[A, C], rie: IsEnum[C]): Decoder[A] =
-    Decoder[String].emap(s => rie.from(s).fold[Either[String, A]](Left("enum"))(c => Right(gen.from(c))))
 }
