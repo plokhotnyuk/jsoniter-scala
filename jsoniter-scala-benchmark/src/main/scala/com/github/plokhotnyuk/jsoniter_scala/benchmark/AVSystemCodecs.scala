7b450caa4bb547889ab4ea1374ad803ca7fe39b6
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2019-01-01 21:11:59 +0100

    Code clean up

--- jsoniter-scala-benchmark/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/benchmark/AVSystemCodecs.scala
+++ jsoniter-scala-benchmark/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/benchmark/AVSystemCodecs.scala
@@ -12,18 +12,8 @@ import scala.collection.mutable
 
 object AVSystemCodecs {
   implicit val adtGenCodec: GenCodec[ADTBase] = materializeRecursively
-  implicit val anyValsGenCodec: GenCodec[AnyVals] = {
-    implicit val byteValGenCodec: GenCodec[ByteVal] = materialize
-    implicit val shortValGenCodec: GenCodec[ShortVal] = materialize
-    implicit val intValGenCodec: GenCodec[IntVal] = materialize
-    implicit val longValGenCodec: GenCodec[LongVal] = materialize
-    implicit val booleanValGenCodec: GenCodec[BooleanVal] = materialize
-    implicit val charValGenCodec: GenCodec[CharVal] = materialize
-    implicit val doubleValGenCodec: GenCodec[DoubleVal] = materialize
-    implicit val floatValGenCodec: GenCodec[FloatVal] = materialize
-    materialize
-  }
-  implicit val anyRefsGenCodec: GenCodec[AnyRefs] = materialize
+  implicit val anyValsGenCodec: GenCodec[AnyVals] = materializeRecursively
+  implicit val anyRefsGenCodec: GenCodec[AnyRefs] = materializeRecursively
   implicit val durationGenCodec: GenCodec[Duration] = transformed(_.toString, Duration.parse)
   implicit val suitEnumGenCodec: GenCodec[SuitEnum] = transformed(_.toString, SuitEnum.withName)
   implicit val instantGenCodec: GenCodec[Instant] = transformed(_.toString, Instant.parse)
@@ -51,17 +41,17 @@ object AVSystemCodecs {
   implicit val zoneOffsetGenCodec: GenCodec[ZoneOffset] = transformed(_.toString, ZoneOffset.of)
   implicit val bitSetGenCodec: GenCodec[BitSet] =
     transformed(_.toArray, (x: Array[Int]) => BitSet(x:_*)) // WARNING: don't do this for open-system
-  implicit val extractFieldsGenCodec: GenCodec[ExtractFields] = materialize
+  implicit val extractFieldsGenCodec: GenCodec[ExtractFields] = materializeRecursively
   implicit val geoJSONGenCodec: GenCodec[GeoJSON] = materializeRecursively
   implicit val googleMapsAPIGenCodec: GenCodec[DistanceMatrix] = materializeRecursively
   implicit val intMapOfBooleansGenCodec: GenCodec[IntMap[Boolean]] =
     transformed(_.seq, (x: Map[Int, Boolean]) => IntMap(x.toArray:_*))
-  implicit val missingReqFieldGenCodec: GenCodec[MissingReqFields] = materialize
-  implicit val mutableBitSetGenCodec: GenCodec[mutable.BitSet] = // WARNING: don't do this for open-system
-    transformed(_.toArray, (x: Array[Int]) => mutable.BitSet(x:_*))
+  implicit val missingReqFieldGenCodec: GenCodec[MissingReqFields] = materializeRecursively
+  implicit val mutableBitSetGenCodec: GenCodec[mutable.BitSet] =
+    transformed(_.toArray, (x: Array[Int]) => mutable.BitSet(x:_*)) // WARNING: don't do this for open-system
   implicit val mutableLongMapOfBooleansGenCodec: GenCodec[mutable.LongMap[Boolean]] =
     transformed(_.seq, (x: mutable.Map[Long, Boolean]) => mutable.LongMap(x.toArray:_*))
-  implicit val nestedStructsGenCodec: GenCodec[NestedStructs] = materialize
-  implicit val primitivesGenCodec: GenCodec[Primitives] = materialize
+  implicit val nestedStructsGenCodec: GenCodec[NestedStructs] = materializeRecursively
+  implicit val primitivesGenCodec: GenCodec[Primitives] = materializeRecursively
   implicit val twitterAPIGenCodec: GenCodec[Tweet] = materializeRecursively
 }
