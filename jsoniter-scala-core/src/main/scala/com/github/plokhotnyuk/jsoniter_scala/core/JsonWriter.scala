0bacb0439d3c4724a40b2fc08f8fa4fc6e8671c4
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2019-04-06 21:50:27 +0200

    More efficient serialization of UUID

--- jsoniter-scala-core/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/core/JsonWriter.scala
+++ jsoniter-scala-core/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/core/JsonWriter.scala
@@ -126,7 +126,7 @@ final class JsonWriter private[jsoniter_scala](
 
   def writeKey(x: UUID): Unit = {
     writeOptionalCommaAndIndentionBeforeKey()
-    writeUUID(x)
+    writeUUID(x.getMostSignificantBits, x.getLeastSignificantBits)
     writeColon()
   }
 
@@ -241,7 +241,7 @@ final class JsonWriter private[jsoniter_scala](
 
   def writeVal(x: UUID): Unit = {
     writeOptionalCommaAndIndentionBeforeValue()
-    writeUUID(x)
+    writeUUID(x.getMostSignificantBits, x.getLeastSignificantBits)
   }
 
   def writeVal(x: String): Unit = {
@@ -627,51 +627,69 @@ final class JsonWriter private[jsoniter_scala](
     pos + 1
   }
 
-  private[this] def writeUUID(x: UUID): Unit = count = {
-    var pos = ensureBufCapacity(38)
+  private[this] def writeUUID(mostSigBits: Long, leastSigBits: Long): Unit = count = {
+    val pos = ensureBufCapacity(38)
     val buf = this.buf
     val ds = hexDigits
+    val mostSigBits1 = (mostSigBits >>> 32).toInt
     buf(pos) = '"'
-    pos = toHex((x.getMostSignificantBits >>> 32).toInt, pos + 1, buf, ds)
-    buf(pos) = '-'
-    pos = toDashedHex(x.getMostSignificantBits.toInt, pos + 1, buf, ds)
-    buf(pos) = '-'
-    pos = toHex(x.getLeastSignificantBits.toInt, toDashedHex((x.getLeastSignificantBits >>> 32).toInt, pos + 1, buf, ds), buf, ds)
-    buf(pos) = '"'
-    pos + 1
-  }
-
-  private[this] def toHex(n: Int, pos: Int, buf: Array[Byte], ds: Array[Short]): Int = {
-    val d1 = ds(n >>> 24)
-    buf(pos) = (d1 >> 8).toByte
-    buf(pos + 1) = d1.toByte
-    val d2 = ds((n >>> 16) & 0xFF)
-    buf(pos + 2) = (d2 >> 8).toByte
-    buf(pos + 3) = d2.toByte
-    val d3 = ds((n >>> 8) & 0xFF)
-    buf(pos + 4) = (d3 >> 8).toByte
-    buf(pos + 5) = d3.toByte
-    val d4 = ds(n & 0xFF)
-    buf(pos + 6) = (d4 >> 8).toByte
-    buf(pos + 7) = d4.toByte
-    pos + 8
-  }
-
-  private[this] def toDashedHex(n: Int, pos: Int, buf: Array[Byte], ds: Array[Short]): Int = {
-    val d1 = ds(n >>> 24)
-    buf(pos) = (d1 >> 8).toByte
-    buf(pos + 1) = d1.toByte
-    val d2 = ds((n >>> 16) & 0xFF)
-    buf(pos + 2) = (d2 >> 8).toByte
-    buf(pos + 3) = d2.toByte
-    buf(pos + 4) = '-'
-    val d3 = ds((n >>> 8) & 0xFF)
+    val d1 = ds(mostSigBits1 >>> 24)
+    buf(pos + 1) = (d1 >> 8).toByte
+    buf(pos + 2) = d1.toByte
+    val d2 = ds((mostSigBits1 >>> 16) & 0xFF)
+    buf(pos + 3) = (d2 >> 8).toByte
+    buf(pos + 4) = d2.toByte
+    val d3 = ds((mostSigBits1 >>> 8) & 0xFF)
     buf(pos + 5) = (d3 >> 8).toByte
     buf(pos + 6) = d3.toByte
-    val d4 = ds(n & 0xFF)
+    val d4 = ds(mostSigBits1 & 0xFF)
     buf(pos + 7) = (d4 >> 8).toByte
     buf(pos + 8) = d4.toByte
-    pos + 9
+    val mostSigBits2 = mostSigBits.toInt
+    buf(pos + 9) = '-'
+    val d5 = ds(mostSigBits2 >>> 24)
+    buf(pos + 10) = (d5 >> 8).toByte
+    buf(pos + 11) = d5.toByte
+    val d6 = ds((mostSigBits2 >>> 16) & 0xFF)
+    buf(pos + 12) = (d6 >> 8).toByte
+    buf(pos + 13) = d6.toByte
+    buf(pos + 14) = '-'
+    val d7 = ds((mostSigBits2 >>> 8) & 0xFF)
+    buf(pos + 15) = (d7 >> 8).toByte
+    buf(pos + 16) = d7.toByte
+    val d8 = ds(mostSigBits2 & 0xFF)
+    buf(pos + 17) = (d8 >> 8).toByte
+    buf(pos + 18) = d8.toByte
+    val leastSigBits1 = (leastSigBits >>> 32).toInt
+    buf(pos + 19) = '-'
+    val d9 = ds(leastSigBits1 >>> 24)
+    buf(pos + 20) = (d9 >> 8).toByte
+    buf(pos + 21) = d9.toByte
+    val d10 = ds((leastSigBits1 >>> 16) & 0xFF)
+    buf(pos + 22) = (d10 >> 8).toByte
+    buf(pos + 23) = d10.toByte
+    buf(pos + 24) = '-'
+    val d11 = ds((leastSigBits1 >>> 8) & 0xFF)
+    buf(pos + 25) = (d11 >> 8).toByte
+    buf(pos + 26) = d11.toByte
+    val d12 = ds(leastSigBits1 & 0xFF)
+    buf(pos + 27) = (d12 >> 8).toByte
+    buf(pos + 28) = d12.toByte
+    val leastSigBits2 = leastSigBits.toInt
+    val d13 = ds(leastSigBits2 >>> 24)
+    buf(pos + 29) = (d13 >> 8).toByte
+    buf(pos + 30) = d13.toByte
+    val d14 = ds((leastSigBits2 >>> 16) & 0xFF)
+    buf(pos + 31) = (d14 >> 8).toByte
+    buf(pos + 32) = d14.toByte
+    val d15 = ds((leastSigBits2 >>> 8) & 0xFF)
+    buf(pos + 33) = (d15 >> 8).toByte
+    buf(pos + 34) = d15.toByte
+    val d16 = ds(leastSigBits2 & 0xFF)
+    buf(pos + 35) = (d16 >> 8).toByte
+    buf(pos + 36) = d16.toByte
+    buf(pos + 37) = '"'
+    pos + 38
   }
 
   private[this] def writeString(s: String): Unit = count = {
