64f07a5207bc5794e8c580ce7a779ab53b1eddcd
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2019-05-10 16:36:58 +0200

    Switch to the first production release of GraalVM

--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sbt -java-home /usr/lib/jvm/graalvm-ee-1.0.0 -no-colors clean 'jsoniter-scala-benchmark/jmh:run -p size=128 -prof gc -rf json -rff graalvmee1.json .*' 2>&1 | tee graalvmee1.txt
-sbt -java-home /usr/lib/jvm/graalvm-ce-1.0.0 -no-colors clean 'jsoniter-scala-benchmark/jmh:run -p size=128 -prof gc -rf json -rff graalvmce1.json .*' 2>&1 | tee graalvmce1.txt
+sbt -java-home /usr/lib/jvm/graalvm-ee-19.0.0 -no-colors clean 'jsoniter-scala-benchmark/jmh:run -p size=128 -prof gc -rf json -rff graalvmee1.json .*' 2>&1 | tee graalvmee1.txt
+sbt -java-home /usr/lib/jvm/graalvm-ce-19.0.0 -no-colors clean 'jsoniter-scala-benchmark/jmh:run -p size=128 -prof gc -rf json -rff graalvmce1.json .*' 2>&1 | tee graalvmce1.txt
 sbt -java-home /usr/lib/jvm/openjdk-11 -no-colors clean 'jsoniter-scala-benchmark/jmh:run -jvm /usr/lib/jvm/openjdk-12/bin/java -p size=128 -prof gc -rf json -rff openjdk12.json .*' 2>&1 | tee openjdk12.txt
 sbt -java-home /usr/lib/jvm/openjdk-11 -no-colors clean 'jsoniter-scala-benchmark/jmh:run -p size=128 -prof gc -rf json -rff openjdk11.json .*' 2>&1 | tee openjdk11.txt
 sbt -java-home /usr/lib/jvm/java-8-openjdk-amd64 -no-colors clean 'jsoniter-scala-benchmark/jmh:run -p size=128 -prof gc -rf json -rff openjdk8.json .*' 2>&1 | tee openjdk8.txt
--- jsoniter-scala-examples/README.md
+++ jsoniter-scala-examples/README.md
@@ -14,7 +14,7 @@ java -jar target/scala-2.13.0-RC1/jsoniter-scala-examples-assembly-0.1.0-SNAPSHO
 ## How to build with a native image and run binaries
 
 ```sh
-/usr/lib/jvm/graalvm-ee-1.0.0/bin/native-image --no-server -H:UnsafeAutomaticSubstitutionsLogLevel=3 -jar target/scala-2.12/jsoniter-scala-examples-assembly-0.1.0-SNAPSHOT.jar
+/usr/lib/jvm/graalvm-ce-19.0.0/bin/native-image --no-server -H:UnsafeAutomaticSubstitutionsLogLevel=3 -jar target/scala-2.12/jsoniter-scala-examples-assembly-0.1.0-SNAPSHOT.jar
 
 ./jsoniter-scala-examples-assembly-0.1.0-SNAPSHOT
 ```
