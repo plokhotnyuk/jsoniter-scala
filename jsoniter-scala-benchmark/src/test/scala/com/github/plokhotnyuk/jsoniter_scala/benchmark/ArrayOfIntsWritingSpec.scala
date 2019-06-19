5327a80de787b0ac495e51e9b38f1ccf0c507fdf
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2019-06-19 15:18:54 +0200

    Update scalikejackson to 0.5.3 + add missing tests for benchmarks

--- jsoniter-scala-benchmark/src/test/scala/com/github/plokhotnyuk/jsoniter_scala/benchmark/ArrayOfIntsWritingSpec.scala
+++ jsoniter-scala-benchmark/src/test/scala/com/github/plokhotnyuk/jsoniter_scala/benchmark/ArrayOfIntsWritingSpec.scala
@@ -15,6 +15,7 @@ class ArrayOfIntsWritingSpec extends BenchmarkSpecBase {
       toString(benchmark.jsoniterJava()) shouldBe benchmark.jsonString
       toString(benchmark.jsoniterScala()) shouldBe benchmark.jsonString
       toString(benchmark.preallocatedBuf, 0, benchmark.jsoniterScalaPrealloc()) shouldBe benchmark.jsonString
+      toString(benchmark.scalikeJackson()) shouldBe benchmark.jsonString
       toString(benchmark.sprayJson()) shouldBe benchmark.jsonString
       toString(benchmark.playJson()) shouldBe benchmark.jsonString
       toString(benchmark.uPickle()) shouldBe benchmark.jsonString
