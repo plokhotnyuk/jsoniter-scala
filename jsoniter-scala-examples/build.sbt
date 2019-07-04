19309071e7b3b023a24aa8038769b8f318092a6c
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2019-07-04 17:19:31 +0200

    Clean up of examples

--- jsoniter-scala-examples/build.sbt
+++ jsoniter-scala-examples/build.sbt
@@ -3,6 +3,7 @@ val `jsoniter-scala-examples` = project.in(file("."))
     scalaVersion := "2.12.8",
     scalacOptions ++= Seq("-Xmacro-settings:print-codecs"),
     crossScalaVersions := Seq("2.13.0", "2.12.8", "2.11.12"),
+    mainClass in assembly := Some("com.github.plokhotnyuk.jsoniter_scala.examples.Example01"),
     libraryDependencies ++= Seq(
       "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-core" % "latest.integration",
       "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % "latest.integration" % Provided // required only in compile-time
--- jsoniter-scala-examples/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/examples/Example01.scala
+++ jsoniter-scala-examples/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/examples/Example01.scala
@@ -3,14 +3,17 @@ package com.github.plokhotnyuk.jsoniter_scala.examples
 import com.github.plokhotnyuk.jsoniter_scala.macros._
 import com.github.plokhotnyuk.jsoniter_scala.core._
 
-case class Device(id: Int, model: String)
+/**
+  * Example of basic usage from README.md
+  */
+object Example01 {
+  case class Device(id: Int, model: String)
 
-case class User(name: String, devices: Seq[Device])
+  case class User(name: String, devices: Seq[Device])
 
-object Examples {
+  implicit val codec: JsonValueCodec[User] = JsonCodecMaker.make(CodecMakerConfig())
+
   def main(args: Array[String]): Unit = {
-    implicit val codec: JsonValueCodec[User] = JsonCodecMaker.make[User](CodecMakerConfig())
-
     val user = readFromArray("""{"name":"John","devices":[{"id":1,"model":"HTC One X"}]}""".getBytes("UTF-8"))
     val json = writeToArray(User(name = "John", devices = Seq(Device(id = 2, model = "iPhone X"))))
 
