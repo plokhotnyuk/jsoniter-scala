c7a4b5e40a6d182ba3b9ee95b5e382ae36c23b5e
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2019-07-08 20:06:31 +0200

    Fix #327 by adding support for literal types (#329)

--- jsoniter-scala-macros/src/test/scala/com/github/plokhotnyuk/jsoniter_scala/macros/JsonCodecMakerSpec.scala
+++ jsoniter-scala-macros/src/test/scala/com/github/plokhotnyuk/jsoniter_scala/macros/JsonCodecMakerSpec.scala
@@ -1,7 +1,6 @@
 package com.github.plokhotnyuk.jsoniter_scala.macros
 
-import java.io.{ByteArrayInputStream, ByteArrayOutputStream, PrintWriter, StringWriter}
-import java.nio.ByteBuffer
+import java.io.{PrintWriter, StringWriter}
 import java.nio.charset.StandardCharsets.UTF_8
 import java.time._
 import java.util.{Objects, UUID}
@@ -9,7 +8,6 @@ import java.util.{Objects, UUID}
 import com.github.plokhotnyuk.jsoniter_scala.core._
 import com.github.plokhotnyuk.jsoniter_scala.macros.JsonCodecMaker.make
 import org.scalatest.exceptions.TestFailedException
-import org.scalatest.{Matchers, WordSpec}
 
 import scala.annotation.{switch, tailrec}
 import scala.language.higherKinds
@@ -27,7 +25,7 @@ object Weapon {
   final case object Sword extends Weapon
 }
 
-class JsonCodecMakerSpec extends WordSpec with Matchers {
+class JsonCodecMakerSpec extends VerifyingSpec {
   case class Primitives(b: Byte, s: Short, i: Int, l: Long, bl: Boolean, ch: Char, dbl: Double, f: Float)
 
   val primitives = Primitives(1.toByte, 2.toShort, 3, 4L, bl = true, 'V', 1.1, 2.2f)
@@ -1776,91 +1774,6 @@ class JsonCodecMakerSpec extends WordSpec with Matchers {
       JsonCodecMaker.simpleClassName("Test") shouldBe "Test"
     }
   }
-
-  def verifySerDeser[T](codec: JsonValueCodec[T], obj: T, json: String, cfg: WriterConfig = WriterConfig()): Unit = {
-    verifySer(codec, obj, json, cfg)
-    verifyDeser(codec, obj, json)
-  }
-
-  def verifySer[T](codec: JsonValueCodec[T], obj: T, json: String, cfg: WriterConfig = WriterConfig()): Unit = {
-    val len = json.getBytes(UTF_8).length
-    verifyDirectByteBufferSer(codec, obj, len, cfg, json)
-    verifyHeapByteBufferSer(codec, obj, len, cfg, json)
-    verifyOutputStreamSer(codec, obj, cfg, json)
-    verifyArraySer(codec, obj, cfg, json)
-  }
-
-  def verifyDeser[T](codec: JsonValueCodec[T], obj: T, json: String): Unit =
-    verifyDeserByCheck[T](codec, json, check = (_: T) shouldBe obj)
-
-  def verifyDeserByCheck[T](codec: JsonValueCodec[T], json: String, check: T => Unit): Unit = {
-    val jsonBytes = json.getBytes(UTF_8)
-    verifyDirectByteBufferDeser(codec, jsonBytes, check)
-    verifyHeapByteBufferDeser(codec, jsonBytes, check)
-    verifyInputStreamDeser(codec, jsonBytes, check)
-    verifyByteArrayDeser(codec, jsonBytes, check)
-  }
-
-  def verifyDeserError[T](codec: JsonValueCodec[T], json: String, msg: String): Unit =
-    verifyDeserError(codec, json.getBytes(UTF_8), msg)
-
-  def verifyDeserError[T](codec: JsonValueCodec[T], jsonBytes: Array[Byte], msg: String): Unit = {
-    assert(intercept[JsonReaderException](verifyDirectByteBufferDeser(codec, jsonBytes, (_: T) => ()))
-      .getMessage.contains(msg))
-    assert(intercept[JsonReaderException](verifyHeapByteBufferDeser(codec, jsonBytes, (_: T) => ()))
-      .getMessage.contains(msg))
-    assert(intercept[JsonReaderException](verifyInputStreamDeser(codec, jsonBytes, (_: T) => ()))
-      .getMessage.contains(msg))
-    assert(intercept[JsonReaderException](verifyByteArrayDeser(codec, jsonBytes, (_: T) => ()))
-      .getMessage.contains(msg))
-  }
-
-  def verifyDirectByteBufferSer[T](codec: JsonValueCodec[T], obj: T, len: Int, cfg: WriterConfig, expectedStr: String): Unit = {
-    val directBuf = ByteBuffer.allocateDirect(len + 100)
-    directBuf.position(0)
-    writeToByteBuffer(obj, directBuf, cfg)(codec)
-    directBuf.position(0)
-    val buf = new Array[Byte](len)
-    directBuf.get(buf)
-    toString(buf) shouldBe expectedStr
-  }
-
-  def verifyHeapByteBufferSer[T](codec: JsonValueCodec[T], obj: T, len: Int, cfg: WriterConfig, expectedStr: String): Unit = {
-    val heapBuf = ByteBuffer.wrap(new Array[Byte](len + 100))
-    heapBuf.position(0)
-    writeToByteBuffer(obj, heapBuf, cfg)(codec)
-    heapBuf.position(0)
-    val buf = new Array[Byte](len)
-    heapBuf.get(buf)
-    toString(buf) shouldBe expectedStr
-  }
-
-  def verifyOutputStreamSer[T](codec: JsonValueCodec[T], obj: T, cfg: WriterConfig, expectedStr: String): Unit = {
-    val baos = new ByteArrayOutputStream
-    writeToStream(obj, baos, cfg)(codec)
-    toString(baos.toByteArray) shouldBe expectedStr
-  }
-
-  def verifyArraySer[T](codec: JsonValueCodec[T], obj: T, cfg: WriterConfig, expectedStr: String): Unit =
-    toString(writeToArray(obj, cfg)(codec)) shouldBe expectedStr
-
-  def verifyDirectByteBufferDeser[T](codec: JsonValueCodec[T], json: Array[Byte], check: T => Unit): Unit = {
-    val directBuf = ByteBuffer.allocateDirect(json.length)
-    directBuf.put(json)
-    directBuf.position(0)
-    check(readFromByteBuffer(directBuf)(codec))
-  }
-
-  def verifyHeapByteBufferDeser[T](codec: JsonValueCodec[T], json: Array[Byte], check: T => Unit): Unit =
-    check(readFromByteBuffer(ByteBuffer.wrap(json))(codec))
-
-  def verifyInputStreamDeser[T](codec: JsonValueCodec[T], json: Array[Byte], check: T => Unit): Unit =
-    check(readFromStream(new ByteArrayInputStream(json))(codec))
-
-  def verifyByteArrayDeser[T](codec: JsonValueCodec[T], json: Array[Byte], check: T => Unit): Unit =
-    check(readFromArray(json)(codec))
-
-  def toString(json: Array[Byte]): String = new String(json, 0, json.length, UTF_8)
 }
 
 case class NameOverridden(@named("new_" + "name") oldName: String) // intentionally declared after the `make` call
