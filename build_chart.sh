554a9c3e1f2b1f21eee901f4faa0338663a6057f
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2019-08-17 08:38:25 +0200

    Add results from OpenJDK 13 and OpenJDK 13 + Graal, remove from comparison OpenJDK 12

--- build_chart.sh
+++ build_chart.sh
@@ -1,11 +1,13 @@
-echo 'var providedBenchmarks = ["OpenJDK 8", "OpenJDK 11", "OpenJDK 12", "GraalVM CE 19", "GraalVM EE 19"];
+echo 'var providedBenchmarks = ["OpenJDK 8", "OpenJDK 11", "OpenJDK 13", "OpenJDK 13 Graal", "GraalVM CE 19", "GraalVM EE 19"];
 var providedBenchmarkStore = {
 "OpenJDK 8":'
 cat openjdk8.json
 echo ', "OpenJDK 11":'
 cat openjdk11.json
-echo ', "OpenJDK 12":'
-cat openjdk12.json
+echo ', "OpenJDK 13":'
+cat openjdk13.json
+echo ', "OpenJDK 13 Graal":'
+cat openjdk13graal.json
 echo ', "GraalVM CE 19":'
 cat graalvmce19.json
 echo ', "GraalVM EE 19":'
