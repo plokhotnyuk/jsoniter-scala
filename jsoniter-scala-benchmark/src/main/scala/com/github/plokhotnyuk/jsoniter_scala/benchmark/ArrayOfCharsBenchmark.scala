fd171120764fba134d498e5a327a0028e3206761
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2019-09-07 12:38:11 +0200

    Fix inefficiency in the benchmark setup

--- jsoniter-scala-benchmark/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/benchmark/ArrayOfCharsBenchmark.scala
+++ jsoniter-scala-benchmark/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/benchmark/ArrayOfCharsBenchmark.scala
@@ -15,7 +15,7 @@ abstract class ArrayOfCharsBenchmark extends CommonParams {
   @Setup
   def setup(): Unit = {
     obj = (1 to size).map(i => (((i * 1498724053) % 10) + 48).toChar).toArray
-    jsonString = "[" + obj.map(x => "\"" + x + "\"").reduce(_ + "," + _) + "]"
+    jsonString = obj.map(_.toString).mkString("[\"", "\",\"", "\"]")
     jsonBytes = jsonString.getBytes(UTF_8)
     preallocatedBuf = new Array[Byte](jsonBytes.length + 100/*to avoid possible out of bounds error*/)
   }
