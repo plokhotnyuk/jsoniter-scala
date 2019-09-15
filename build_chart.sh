9c39663048b5082121825b676516f0c456dbce53
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2019-09-15 08:44:44 +0200

    Add results from Eclipse OpenJ9 and Azul Zing

--- build_chart.sh
+++ build_chart.sh
@@ -1,6 +1,8 @@
-echo 'var providedBenchmarks = ["OpenJDK 8", "OpenJDK 11", "OpenJDK 13", "OpenJDK 13 Graal", "GraalVM CE 19", "GraalVM EE 19"];
+echo 'var providedBenchmarks = ["OpenJ9 JDK 11", "OpenJDK 8", "OpenJDK 11", "OpenJDK 13", "OpenJDK 13 Graal", "GraalVM CE 19", "GraalVM EE 19", "Zing JDK 11"];
 var providedBenchmarkStore = {
-"OpenJDK 8":'
+"OpenJ9 JDK 11":'
+cat openj9jdk11.json
+echo ', "OpenJDK 8":'
 cat openjdk8.json
 echo ', "OpenJDK 11":'
 cat openjdk11.json
@@ -12,4 +14,6 @@ echo ', "GraalVM CE 19":'
 cat graalvmce19.json
 echo ', "GraalVM EE 19":'
 cat graalvmee19.json
+echo ', "Zing JDK 11":'
+cat zingjdk11.json
 echo '}'
