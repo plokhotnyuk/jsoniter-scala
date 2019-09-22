209288b060df208833a5941b1915d9354c3188e3
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2019-09-22 15:11:43 +0200

    Update results for jsoniter-scala 0.55.2

--- build_chart.sh
+++ build_chart.sh
@@ -1,8 +1,6 @@
-echo 'var providedBenchmarks = ["OpenJ9 JDK 11", "OpenJDK 8", "OpenJDK 11", "OpenJDK 13", "OpenJDK 13 Graal", "GraalVM CE 19", "GraalVM EE 19", "Zing JDK 11"];
+echo 'var providedBenchmarks = ["OpenJDK 8", "OpenJDK 11", "OpenJDK 13", "OpenJDK 13 Graal", "GraalVM CE 19", "GraalVM EE 19"];
 var providedBenchmarkStore = {
-"OpenJ9 JDK 11":'
-cat openj9jdk11.json
-echo ', "OpenJDK 8":'
+"OpenJDK 8":'
 cat openjdk8.json
 echo ', "OpenJDK 11":'
 cat openjdk11.json
@@ -14,6 +12,4 @@ echo ', "GraalVM CE 19":'
 cat graalvmce19.json
 echo ', "GraalVM EE 19":'
 cat graalvmee19.json
-echo ', "Zing JDK 11":'
-cat zingjdk11.json
 echo '}'
