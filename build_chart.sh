5b67a5681f344115cc05bd1598f24821333dd1e7
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2019-11-21 20:26:09 +0100

    Update results for jsoniter-scala 2.0.2-SNAPSHOT on mac

--- build_chart.sh
+++ build_chart.sh
@@ -1,15 +1,19 @@
-echo 'var providedBenchmarks = ["OpenJDK 8", "OpenJDK 11", "OpenJDK 13", "OpenJDK 13 Graal", "GraalVM CE 19", "GraalVM EE 19"];
+echo 'var providedBenchmarks = ["Corretto 8", "Corretto 11", "OpenJDK 13", "OpenJDK 13 Graal", "GraalVM CE 8", "GraalVM CE 11", "GraalVM EE 8", "GraalVM EE 11"];
 var providedBenchmarkStore = {
-"OpenJDK 8":'
-cat openjdk8.json
-echo ', "OpenJDK 11":'
-cat openjdk11.json
+"Corretto 8":'
+cat corretto-8.json
+echo ', "Corretto 11":'
+cat corretto-11.json
 echo ', "OpenJDK 13":'
-cat openjdk13.json
+cat openjdk-13.json
 echo ', "OpenJDK 13 Graal":'
-cat openjdk13graal.json
-echo ', "GraalVM CE 19":'
-cat graalvmce19.json
-echo ', "GraalVM EE 19":'
-cat graalvmee19.json
+cat openjdk-13-graal.json
+echo ', "GraalVM CE 8":'
+cat graalvm-ce-java8.json
+echo ', "GraalVM CE 11":'
+cat graalvm-ce-java11.json
+echo ', "GraalVM EE 8":'
+cat graalvm-ee-java8.json
+echo ', "GraalVM EE 11":'
+cat graalvm-ee-java11.json
 echo '}'
