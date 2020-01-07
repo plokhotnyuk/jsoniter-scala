0e99208655bce5eca169ba212bcd672487db4a49
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2020-01-07 07:50:19 +0100

    Results for jsoniter-scala 2.1.2 + update OpenJDK to 15 (ea build)

--- build_chart.sh
+++ build_chart.sh
@@ -1,13 +1,13 @@
-echo 'var providedBenchmarks = ["Corretto 8", "Corretto 11", "OpenJDK 13", "OpenJDK 13 Graal", "GraalVM CE 8", "GraalVM CE 11", "GraalVM EE 8", "GraalVM EE 11"];
+echo 'var providedBenchmarks = ["Corretto 8", "Corretto 11", "OpenJDK 15", "OpenJDK 15 Graal", "GraalVM CE 8", "GraalVM CE 11", "GraalVM EE 8", "GraalVM EE 11"];
 var providedBenchmarkStore = {
 "Corretto 8":'
 cat corretto-8.json
 echo ', "Corretto 11":'
 cat corretto-11.json
-echo ', "OpenJDK 13":'
-cat openjdk-13.json
-echo ', "OpenJDK 13 Graal":'
-cat openjdk-13-graal.json
+echo ', "OpenJDK 15":'
+cat openjdk-15.json
+echo ', "OpenJDK 15 Graal":'
+cat openjdk-15-graal.json
 echo ', "GraalVM CE 8":'
 cat graalvm-ce-java8.json
 echo ', "GraalVM CE 11":'
