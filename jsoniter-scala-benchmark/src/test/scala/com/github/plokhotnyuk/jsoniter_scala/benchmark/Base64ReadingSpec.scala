30b0e499763dca8b7ab3f80b4a82f8e571da978e
Doug Roper <doug.roper@rallyhealth.com>
2020-02-06 02:52:20 -0500

    v1.0.1 and Base64Reading

--- jsoniter-scala-benchmark/src/test/scala/com/github/plokhotnyuk/jsoniter_scala/benchmark/Base64ReadingSpec.scala
+++ jsoniter-scala-benchmark/src/test/scala/com/github/plokhotnyuk/jsoniter_scala/benchmark/Base64ReadingSpec.scala
@@ -13,8 +13,7 @@ class Base64ReadingSpec extends BenchmarkSpecBase {
       benchmark.dslJsonScala() shouldBe benchmark.obj
       benchmark.jacksonScala() shouldBe benchmark.obj
       benchmark.jsoniterScala() shouldBe benchmark.obj
-      //FIXME: weePickle throws com.rallyhealth.weepickle.v1.core.Abort: expected sequence got string
-      //benchmark.weePickle() shouldBe benchmark.obj
+      benchmark.weePickle() shouldBe benchmark.obj
     }
   }
 }
