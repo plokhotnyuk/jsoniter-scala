6e0f2e1ee39e6e26da71833fcf738ec332916de3
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2020-03-23 05:37:10 +0100

    Try to reproduce missing commas

--- jsoniter-scala-examples/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/examples/Example01.scala
+++ jsoniter-scala-examples/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/examples/Example01.scala
@@ -7,17 +7,9 @@ import com.github.plokhotnyuk.jsoniter_scala.core._
   * Example of basic usage from README.md
   */
 object Example01 {
-  case class Device(id: Int, model: String)
+  implicit val seqCodec: JsonValueCodec[List[Int]] =
+    JsonCodecMaker.make(CodecMakerConfig)
 
-  case class User(name: String, devices: Seq[Device])
-
-  implicit val codec: JsonValueCodec[User] = JsonCodecMaker.make(CodecMakerConfig)
-
-  def main(args: Array[String]): Unit = {
-    val user = readFromArray("""{"name":"John","devices":[{"id":1,"model":"HTC One X"}]}""".getBytes("UTF-8"))
-    val json = writeToArray(User(name = "John", devices = Seq(Device(id = 2, model = "iPhone X"))))
-
-    println(user)
-    println(new String(json, "UTF-8"))
-  }
+  def main(args: Array[String]): Unit =
+    Console.out.write(writeToArray(List(1, 2, 3, 4, 5)))
 }
