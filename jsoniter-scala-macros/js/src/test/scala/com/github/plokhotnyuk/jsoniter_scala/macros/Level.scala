32c56861b37566e79b4513934926a14ed27a0726
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2020-05-14 16:37:36 +0200

    Try to emulate Java enums with Scala.js

---
+++ jsoniter-scala-macros/js/src/test/scala/com/github/plokhotnyuk/jsoniter_scala/macros/Level.scala
@@ -0,0 +1,9 @@
+package com.github.plokhotnyuk.jsoniter_scala.macros
+
+object Level {
+  lazy val HIGH: Level = new Level("HIGH", 0)
+  lazy val LOW: Level = new Level("LOW", 1)
+  lazy val values: Array[Level] = Array(HIGH, LOW)
+}
+
+final class Level(name: String, ordinal: Int) extends Enum[Level](name, ordinal)
