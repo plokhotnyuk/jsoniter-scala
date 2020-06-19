7cf547cfb13dfacd3be3759b603426432202a5b9
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2020-06-19 15:11:58 +0200

    Add a custom codec for `java.time.ZonedDateTime` with alternative encoding of zero seconds

--- jsoniter-scala-macros/shared/src/test/scala/com/github/plokhotnyuk/jsoniter_scala/macros/JsonCodecMakerSpec.scala
+++ jsoniter-scala-macros/shared/src/test/scala/com/github/plokhotnyuk/jsoniter_scala/macros/JsonCodecMakerSpec.scala
@@ -563,6 +563,26 @@ class JsonCodecMakerSpec extends VerifyingSpec {
         "[\"-Infinity\",\"Infinity\",0.0,1.0E10]")
       verifyDeserError(codecOfDoubleArray, "[\"Inf\",\"-Inf\"]", "illegal double, offset: 0x00000005")
     }
+    "serialize date time values into alternative format" in {
+      implicit val customCodecOfZonedDateTime: JsonValueCodec[ZonedDateTime] = new JsonValueCodec[ZonedDateTime] {
+        private[this] val standardCodec: JsonValueCodec[ZonedDateTime] = JsonCodecMaker.make[ZonedDateTime]
+
+        val nullValue: ZonedDateTime = null
+
+        def decodeValue(in: JsonReader, default: ZonedDateTime): ZonedDateTime = in.readZonedDateTime(default)
+
+        def encodeValue(x: ZonedDateTime, out: JsonWriter): _root_.scala.Unit =
+          if (x.getSecond != 0 || x.getNano != 0) out.writeVal(x)
+          else {
+            val s = writeToString(x)(standardCodec)
+            val i = s.indexOf(':', 13) + 3
+            out.writeNonEscapedAsciiVal(s"${s.substring(1, i)}:00${s.substring(i, s.length - 1)}")
+          }
+      }
+      verifySerDeser(make[Array[ZonedDateTime]],
+        _root_.scala.Array(ZonedDateTime.parse("2020-04-10T10:07:00Z"), ZonedDateTime.parse("2020-04-10T10:07:01Z")),
+        "[\"2020-04-10T10:07:00Z\",\"2020-04-10T10:07:01Z\"]")
+    }
     "serialize and deserialize outer types using custom value codecs for opaque types" in {
       abstract class Foo {
         type Bar
