96ed6d282d87d7844e3a9880da4c41a0518f0cc6
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2020-06-19 09:28:21 +0200

    Remove code duplication

--- jsoniter-scala-core/jvm/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/core/JsonWriter.scala
+++ jsoniter-scala-core/jvm/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/core/JsonWriter.scala
@@ -13,65 +13,6 @@ import com.github.plokhotnyuk.jsoniter_scala.core.JsonWriter._
 import scala.annotation.tailrec
 import scala.{specialized => sp}
 
-/**
-  * Configuration for [[com.github.plokhotnyuk.jsoniter_scala.core.JsonWriter]] that contains params for formatting of
-  * output JSON and for tuning of preferred size for internal byte buffer that is created on the writer instantiation
-  * and reused in runtime for serialization of messages using [[java.io.OutputStream]] or [[java.nio.DirectByteBuffer]].
-  * <br/>
-  * All configuration params already initialized to default values, but in some cases they should be altered:
-  * <ul>
-  * <li>turn on pretty printing by specifying of indention step that is greater than 0</li>
-  * <li>turn on escaping of Unicode characters to serialize with only ASCII characters</li>
-  * <li>increase preferred size of an internal byte buffer to reduce allocation rate of grown and then reduced buffers
-  * when writing to [[java.io.OutputStream]] or [[java.nio.DirectByteBuffer]] lot of large (>16Kb)
-  * [[scala.math.BigDecimal]], [[scala.math.BigInt]] or other non escaped ASCII strings written using
-  * `JsonWriter.writeNonEscapedAsciiKey` or `JsonWriter.writeNonEscapedAsciiVal` </li>
-  * </ul>
-  * @param throwWriterExceptionWithStackTrace a flag that allows to turn on a stack traces for debugging purposes in
-  *                                           development
-  * @param indentionStep a size of indention for pretty-printed formatting or 0 for compact output
-  * @param escapeUnicode a flag to turn on hexadecimal escaping of all non-ASCII chars
-  * @param preferredBufSize a preferred size (in bytes) of an internal byte buffer when writing to
-  *                         [[java.io.OutputStream]] or [[java.nio.DirectByteBuffer]]
-  */
-class WriterConfig private (
-    val throwWriterExceptionWithStackTrace: Boolean,
-    val indentionStep: Int,
-    val escapeUnicode: Boolean,
-    val preferredBufSize: Int) {
-  def withThrowWriterExceptionWithStackTrace(throwWriterExceptionWithStackTrace: Boolean): WriterConfig =
-    copy(throwWriterExceptionWithStackTrace = throwWriterExceptionWithStackTrace)
-
-  def withIndentionStep(indentionStep: Int): WriterConfig = {
-    if (indentionStep < 0) throw new IllegalArgumentException("'indentionStep' should be not less than 0")
-    copy(indentionStep = indentionStep)
-  }
-
-  def withEscapeUnicode(escapeUnicode: Boolean): WriterConfig =
-    copy(escapeUnicode = escapeUnicode)
-
-  def withPreferredBufSize(preferredBufSize: Int): WriterConfig = {
-    if (preferredBufSize <= 0) throw new IllegalArgumentException("'preferredBufSize' should be not less than 1")
-    copy(preferredBufSize = preferredBufSize)
-  }
-
-  private[this] def copy(throwWriterExceptionWithStackTrace: Boolean = throwWriterExceptionWithStackTrace,
-                         indentionStep: Int = indentionStep,
-                         escapeUnicode: Boolean = escapeUnicode,
-                         preferredBufSize: Int = preferredBufSize): WriterConfig =
-    new WriterConfig(
-      throwWriterExceptionWithStackTrace = throwWriterExceptionWithStackTrace,
-      indentionStep = indentionStep,
-      escapeUnicode = escapeUnicode,
-      preferredBufSize = preferredBufSize)
-}
-
-object WriterConfig extends WriterConfig(
-  throwWriterExceptionWithStackTrace = false,
-  indentionStep = 0,
-  escapeUnicode = false,
-  preferredBufSize = 16384)
-
 class JsonWriterException private[jsoniter_scala](msg: String, cause: Throwable, withStackTrace: Boolean)
   extends RuntimeException(msg, cause, true, withStackTrace)
 
---
+++ jsoniter-scala-core/shared/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/core/ReaderConfig.scala
@@ -0,0 +1,76 @@
+package com.github.plokhotnyuk.jsoniter_scala.core
+
+/**
+  * Configuration for [[com.github.plokhotnyuk.jsoniter_scala.core.JsonReader]] that contains flags for tuning of
+  * parsing exceptions and preferred sizes for internal buffers that are created on the reader instantiation and reused
+  * in runtime for parsing of messages.
+  * <br/>
+  * All configuration params already initialized by recommended default values, but in some cases they should be altered
+  * for performance reasons:
+  * <ul>
+  * <li>turn off stack traces for parsing exceptions to greatly reduce impact on performance for cases when exceptions
+  * can be not exceptional (e.g. under DoS attacks over open to the world systems), see more details here:
+  * [[https://shipilev.net/blog/2014/exceptional-performance/]]</li>
+  * <li>turn off appending of hex dump to minimize length of exception message</li>
+  * <li>increase preferred size of an internal byte buffer for parsing from [[java.io.InputStream]] or
+  * [[java.nio.DirectByteBuffer]] to reduce allocation rate of grown and then reduced buffers during parsing of large
+  * (>16Kb) numbers (including stringified), raw values, or ADT instances with the discriminator field doesn't
+  * appear in the beginning of the JSON object</li>
+  * <li>increase preferred size of an internal char buffer to reduce allocation rate of grown and then reduced
+  * buffers when large (>4Kb) string instances need to be parsed including those one which use Base16 or Base64
+  * encodings</li>
+  * </ul>
+  * @param throwReaderExceptionWithStackTrace a flag that allows to turn on a stack traces for debugging purposes in
+  *                                           development
+  * @param appendHexDumpToParseException a flag that allows to turn off hex dumping of affected by error part of
+  *                                      an internal byte buffer
+  * @param preferredBufSize a preferred size (in bytes) of an internal byte buffer when parsing from
+  *                         [[java.io.InputStream]]
+  * @param preferredCharBufSize a preferred size (in chars) of an internal char buffer for parsing of string values
+  * @param checkForEndOfInput a flag to check and raise an error if some non whitespace bytes will be detected after
+  *                           successful parsing of the value
+  */
+class ReaderConfig private (
+                             val throwReaderExceptionWithStackTrace: Boolean,
+                             val appendHexDumpToParseException: Boolean,
+                             val preferredBufSize: Int,
+                             val preferredCharBufSize: Int,
+                             val checkForEndOfInput: Boolean) {
+  def withThrowReaderExceptionWithStackTrace(throwReaderExceptionWithStackTrace: Boolean): ReaderConfig =
+    copy(throwReaderExceptionWithStackTrace = throwReaderExceptionWithStackTrace)
+
+  def withAppendHexDumpToParseException(appendHexDumpToParseException: Boolean): ReaderConfig =
+    copy(appendHexDumpToParseException = appendHexDumpToParseException)
+
+  def withPreferredBufSize(preferredBufSize: Int): ReaderConfig = {
+    if (preferredBufSize < 12) throw new IllegalArgumentException("'preferredBufSize' should be not less than 12")
+    copy(preferredBufSize = preferredBufSize)
+  }
+
+  def withPreferredCharBufSize(preferredCharBufSize: Int): ReaderConfig = {
+    if (preferredCharBufSize < 0) throw new IllegalArgumentException("'preferredCharBufSize' should be not less than 0")
+    copy(preferredCharBufSize = preferredCharBufSize)
+  }
+
+  def withCheckForEndOfInput(checkForEndOfInput: Boolean): ReaderConfig =
+    copy(checkForEndOfInput = checkForEndOfInput)
+
+  private[this] def copy(throwReaderExceptionWithStackTrace: Boolean = throwReaderExceptionWithStackTrace,
+                         appendHexDumpToParseException: Boolean = appendHexDumpToParseException,
+                         preferredBufSize: Int = preferredBufSize,
+                         preferredCharBufSize: Int = preferredCharBufSize,
+                         checkForEndOfInput: Boolean = checkForEndOfInput): ReaderConfig =
+    new ReaderConfig(
+      throwReaderExceptionWithStackTrace = throwReaderExceptionWithStackTrace,
+      appendHexDumpToParseException = appendHexDumpToParseException,
+      preferredBufSize = preferredBufSize,
+      preferredCharBufSize = preferredCharBufSize,
+      checkForEndOfInput = checkForEndOfInput)
+}
+
+object ReaderConfig extends ReaderConfig(
+  throwReaderExceptionWithStackTrace = false,
+  appendHexDumpToParseException = true,
+  preferredBufSize = 16384,
+  preferredCharBufSize = 4096,
+  checkForEndOfInput = true)
