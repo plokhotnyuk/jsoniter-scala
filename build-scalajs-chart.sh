a8b4a8416c41e069dd5d9a9707b73ab4b0c72c47
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2020-07-04 08:42:10 +0200

    Publish Scala.js benchmark for 2.4.5-SNAPSHOT

---
+++ build-scalajs-chart.sh
@@ -0,0 +1,13 @@
+echo 'var providedBenchmarks = ["Chrome", "Edge", "Firefox", "Opera", "Safari"];
+var providedBenchmarkStore = {
+"Chrome":'
+cat chrome.json
+echo ', "Edge":'
+cat edge.json
+echo ', "Firefox":'
+cat firefox.json
+echo ', "Opera":'
+cat opera.json
+echo ', "Safari":'
+cat safari.json
+echo '}'
