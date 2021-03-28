3ebbc00a227122d0ab788265073ad8c6634eb52e
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2021-03-28 12:33:01 +0200

    Update results of JVM benchmarks for jsoniter-scala 2.6.5-SNAPSHOT

--- build_chart.sh
+++ build_chart.sh
@@ -1,4 +1,4 @@
-echo 'var providedBenchmarks = ["Corretto 8", "Corretto 11", "OpenJDK 17", "GraalVM CE 8", "GraalVM CE 11", "GraalVM EE 8", "GraalVM EE 11"];
+echo 'var providedBenchmarks = ["Corretto 8", "Corretto 11", "OpenJDK 17", "GraalVM CE 11", "GraalVM CE 16", "GraalVM EE 8", "GraalVM EE 11"];
 var providedBenchmarkStore = {
 "Corretto 8":'
 cat corretto-8.json
@@ -6,10 +6,10 @@ echo ', "Corretto 11":'
 cat corretto-11.json
 echo ', "OpenJDK 17":'
 cat openjdk-17.json
-echo ', "GraalVM CE 8":'
-cat graalvm-ce-java8.json
 echo ', "GraalVM CE 11":'
 cat graalvm-ce-java11.json
+echo ', "GraalVM CE 16":'
+cat graalvm-ce-java16.json
 echo ', "GraalVM EE 8":'
 cat graalvm-ee-java8.json
 echo ', "GraalVM EE 11":'
