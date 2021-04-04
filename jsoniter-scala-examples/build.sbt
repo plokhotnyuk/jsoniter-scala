735cc2c5abcd79afb23345dd2b74133ecdca9255
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2021-04-04 13:34:24 +0200

    Try to reproduce "match may not be exhaustive"

--- jsoniter-scala-examples/build.sbt
+++ jsoniter-scala-examples/build.sbt
@@ -2,13 +2,12 @@ val `jsoniter-scala-examples` = project.in(file("."))
   .enablePlugins(NativeImagePlugin)
   .settings(
     scalaVersion := "2.13.5",
-    scalacOptions ++= Seq("-Xmacro-settings:print-codecs"),
     crossScalaVersions := Seq("2.13.5", "2.12.13", "2.11.12"),
     Compile / mainClass := Some("com.github.plokhotnyuk.jsoniter_scala.examples.Example01"),
     assembly / mainClass := Some("com.github.plokhotnyuk.jsoniter_scala.examples.Example01"),
     libraryDependencies ++= Seq(
-      "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-core" % "latest.integration",
+      "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-core" % "2.7.0",
       // Use the "provided" scope instead when the "compile-internal" scope is not supported
-      "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % "latest.integration" % "compile-internal"
+      "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % "2.7.0" % "compile-internal"
     )
   )
--- jsoniter-scala-examples/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/examples/Example01.scala
+++ jsoniter-scala-examples/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/examples/Example01.scala
@@ -1,21 +1,18 @@
 package com.github.plokhotnyuk.jsoniter_scala.examples
 
-import com.github.plokhotnyuk.jsoniter_scala.macros._
-import com.github.plokhotnyuk.jsoniter_scala.core._
-
 /**
   * Example of basic usage from README.md
   */
 object Example01 {
-  case class Device(id: Int, model: String)
+  import com.github.plokhotnyuk.jsoniter_scala.core._
+  import com.github.plokhotnyuk.jsoniter_scala.macros._
 
-  case class User(name: String, devices: Seq[Device])
-
-  implicit val codec: JsonValueCodec[User] = JsonCodecMaker.make
+  case class Foo(id: Int)
+  implicit val foo: JsonValueCodec[Foo] = JsonCodecMaker.make
 
   def main(args: Array[String]): Unit = {
-    val user = readFromArray("""{"name":"John","devices":[{"id":1,"model":"HTC One X"}]}""".getBytes("UTF-8"))
-    val json = writeToArray(User(name = "John", devices = Seq(Device(id = 2, model = "iPhone X"))))
+    val user = readFromArray("""{"id":1}""".getBytes("UTF-8"))
+    val json = writeToArray(Foo(1))
 
     println(user)
     println(new String(json, "UTF-8"))
