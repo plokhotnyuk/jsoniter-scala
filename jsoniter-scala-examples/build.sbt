bb069917c63c18e01be6c0fb336ad63a4cca9226
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2021-04-07 08:52:18 +0200

    Codec for @yankun1992

--- jsoniter-scala-examples/build.sbt
+++ jsoniter-scala-examples/build.sbt
@@ -7,8 +7,7 @@ val `jsoniter-scala-examples` = project.in(file("."))
     Compile / mainClass := Some("com.github.plokhotnyuk.jsoniter_scala.examples.Example01"),
     assembly / mainClass := Some("com.github.plokhotnyuk.jsoniter_scala.examples.Example01"),
     libraryDependencies ++= Seq(
-      "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-core" % "latest.integration",
-      // Use the "provided" scope instead when the "compile-internal" scope is not supported
-      "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % "latest.integration" % "compile-internal"
+      "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-core" % "2.7.1",
+      "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % "2.7.1" % "provided"
     )
   )
--- jsoniter-scala-examples/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/examples/Example01.scala
+++ jsoniter-scala-examples/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/examples/Example01.scala
@@ -1,23 +1,91 @@
 package com.github.plokhotnyuk.jsoniter_scala.examples
 
-import com.github.plokhotnyuk.jsoniter_scala.macros._
-import com.github.plokhotnyuk.jsoniter_scala.core._
-
-/**
-  * Example of basic usage from README.md
-  */
 object Example01 {
-  case class Device(id: Int, model: String)
-
-  case class User(name: String, devices: Seq[Device])
-
-  implicit val codec: JsonValueCodec[User] = JsonCodecMaker.make
-
   def main(args: Array[String]): Unit = {
-    val user = readFromArray("""{"name":"John","devices":[{"id":1,"model":"HTC One X"}]}""".getBytes("UTF-8"))
-    val json = writeToArray(User(name = "John", devices = Seq(Device(id = 2, model = "iPhone X"))))
+    import com.github.plokhotnyuk.jsoniter_scala.macros._
+    import com.github.plokhotnyuk.jsoniter_scala.core._
+    import java.nio.ByteBuffer
 
-    println(user)
-    println(new String(json, "UTF-8"))
+    case class TushareResponse(
+                                request_id: String,
+                                code: Int,
+                                msg: String,
+                                data: Data)
+
+    object TushareResponse {
+      implicit val codec: JsonValueCodec[TushareResponse] = JsonCodecMaker.make
+    }
+
+    case class Data(
+                     fields: Array[String],
+                     items: Array[Array[Option[String]]],
+                     has_more: Boolean)
+
+    object Data {
+      implicit val codec: JsonValueCodec[Data] = {
+        implicit val optStringCodec: JsonValueCodec[Option[String]] = new JsonValueCodec[Option[String]] {
+          override def decodeValue(in: JsonReader, default: Option[String]): Option[String] = {
+            val b = in.nextToken()
+            if (b == 'n') {
+              in.readNullOrError(None, "expected `null` value")
+              None
+            } else if (b == '"') {
+              in.rollbackToken()
+              Some(in.readString(null))
+            } else if (b == 't' || b == 'f') {
+              in.rollbackToken()
+              Some(in.readBoolean().toString)
+            } else if ((b >= '0' && b <= '9') || b == '-') {
+              in.rollbackToken()
+              Some(in.readBigDecimal(null).toString)
+            } else in.decodeError("unexpected value")
+          }
+
+          override def encodeValue(x: Option[String], out: JsonWriter): Unit =
+            x.fold(out.writeNull())(out.writeVal)
+
+          override def nullValue: Option[String] = None
+        }
+        JsonCodecMaker.make
+      }
+    }
+
+    val response = readFromByteBuffer[TushareResponse](ByteBuffer.wrap(
+    """{
+       |    "request_id": "",
+       |    "code": 0,
+       |    "msg": "",
+       |    "data": {
+       |        "fields": [
+       |            "exchange",
+       |            "cal_date",
+       |            "is_open",
+       |            "pretrade_date"
+       |        ],
+       |        "items": [
+       |            [
+       |                "SSE",
+       |                "19901219",
+       |                1,
+       |                null
+       |            ],
+       |            [
+       |                "SSE",
+       |                "19941222",
+       |                1,
+       |                "19941221"
+       |            ],
+       |            [
+       |                "SSE",
+       |                "19941223",
+       |                1,
+       |                "19941222"
+       |            ]
+       |        ],
+       |        "has_more": false
+       |    }
+       |}""".stripMargin.getBytes("UTF-8")))
+
+    println(writeToString(response, WriterConfig.withIndentionStep(4)))
   }
 }
