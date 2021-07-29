63ee650556e84c3bc09153463ab66d475c32ae32
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2021-07-29 14:59:34 +0200

    Update results from JVMs

--- build_chart.sh
+++ build_chart.sh
@@ -1,11 +1,11 @@
-echo 'var providedBenchmarks = ["Corretto 8", "Corretto 11", "OpenJDK 17", "GraalVM CE 11", "GraalVM CE 16", "GraalVM EE 8", "GraalVM EE 11", "GraalVM EE 16"];
+echo 'var providedBenchmarks = ["Corretto 8", "Corretto 11", "OpenJDK 18", "GraalVM CE 11", "GraalVM CE 16", "GraalVM EE 8", "GraalVM EE 11", "GraalVM EE 16"];
 var providedBenchmarkStore = {
 "Corretto 8":'
 jq -c . < corretto-8.json
 echo ', "Corretto 11":'
 jq -c . < corretto-11.json
-echo ', "OpenJDK 17":'
-jq -c . < openjdk-17.json
+echo ', "OpenJDK 18":'
+jq -c . < openjdk-18.json
 echo ', "GraalVM CE 11":'
 jq -c . < graalvm-ce-java11.json
 echo ', "GraalVM CE 16":'
