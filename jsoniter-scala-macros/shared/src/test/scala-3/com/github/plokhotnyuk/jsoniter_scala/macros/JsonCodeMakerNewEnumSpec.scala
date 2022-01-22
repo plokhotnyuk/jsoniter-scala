47d7edf4f0d953e97a39a2f25edda2fd2dd6ec8f
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2022-01-22 17:36:07 +0200

    Added support for plain scala3 Enum ADT (#849)
    support scala3 non-adt enums with parameters

--- jsoniter-scala-macros/shared/src/test/scala-3/com/github/plokhotnyuk/jsoniter_scala/macros/JsonCodeMakerNewEnumSpec.scala
+++ jsoniter-scala-macros/shared/src/test/scala-3/com/github/plokhotnyuk/jsoniter_scala/macros/JsonCodeMakerNewEnumSpec.scala
@@ -24,8 +24,30 @@ enum Color(val rgb: Int):
   case Green extends Color(0x00FF00)
   case Blue  extends Color(0x0000FF)
 
+
+enum ColorADT(val rgb: Int):
+  case Red   extends ColorADT(0xFF0000)
+  case Green extends ColorADT(0x00FF00)
+  case Blue  extends ColorADT(0x0000FF)
+  case Mix(mix: Int) extends ColorADT(mix)
+
+
+enum Planet(mass: Double, radius: Double):
+  private final val G = 6.67300E-11
+  def surfaceGravity = G * mass / (radius * radius)
+  def surfaceWeight(otherMass: Double) = otherMass * surfaceGravity
+
+  case Mercury extends Planet(3.303e+23, 2.4397e6)
+  case Venus   extends Planet(4.869e+24, 6.0518e6)
+  case Earth   extends Planet(5.976e+24, 6.37814e6)
+  case Mars    extends Planet(6.421e+23, 3.3972e6)
+  case Jupiter extends Planet(1.9e+27,   7.1492e7)
+  case Saturn  extends Planet(5.688e+26, 6.0268e7)
+  case Uranus  extends Planet(8.686e+25, 2.5559e7)
+  case Neptune extends Planet(1.024e+26, 2.4746e7)
+end Planet
+
 // TODO:
-//   Enum ADT  (Color from example)
 //   Enum ADT with type parameters
 //   ordinal flag (create config param)
 
@@ -55,6 +77,20 @@ class JsonCodecMakerEnumSpec extends VerifyingSpec {
     "serialize and deserialize Scala3 enums with parameters" in {
       verifySerDeser(make[List[Color]](CodecMakerConfig),
           List(Color.Red, Color.Red, Color.Green, Color.Blue), """["Red","Red","Green","Blue"]""")
+
     }
+
+    "serialize and deserialize Scala3 enums with multiple parameters" in {
+      verifySerDeser(make[List[Planet]](CodecMakerConfig),
+          List(Planet.Mercury, Planet.Mars), """["Mercury","Mars"]""")
+
+    }
+
+    "serialize and deserialize Scala3 enums ADT" in {
+      verifySerDeser(make[List[ColorADT]](CodecMakerConfig),
+          List(ColorADT.Red, ColorADT.Green, ColorADT.Mix(0)), """["Red","Green",{"type":"Mix","mix":0}]""")
+    }
+
   }
+ 
 }
