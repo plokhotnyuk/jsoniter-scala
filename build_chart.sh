14a82f35e72ce541117778a854308f474b1a0655
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2022-01-24 02:56:34 +0100

    Update results of benchmarks for jsoniter-scala 2.12.0

--- build_chart.sh
+++ build_chart.sh
@@ -1,19 +1,19 @@
-echo 'var providedBenchmarks = ["Corretto 8", "Corretto 11", "OpenJDK 18", "GraalVM CE 11", "GraalVM CE 16", "GraalVM EE 8", "GraalVM EE 11", "GraalVM EE 16"];
+echo 'var providedBenchmarks = ["Zulu 8", "Zulu 11", "Zulu 17", "GraalVM CE 11", "GraalVM CE 17", "GraalVM EE 8", "GraalVM EE 11", "GraalVM EE 17"];
 var providedBenchmarkStore = {
-"Corretto 8":'
-jq -c . < corretto-8.json
-echo ', "Corretto 11":'
-jq -c . < corretto-11.json
-echo ', "OpenJDK 18":'
-jq -c . < openjdk-18.json
+"Zulu 8":'
+jq -c . < zulu-8.json
+echo ', "Zulu 11":'
+jq -c . < zulu-11.json
+echo ', "Zulu 17":'
+jq -c . < zulu-17.json
 echo ', "GraalVM CE 11":'
 jq -c . < graalvm-ce-java11.json
-echo ', "GraalVM CE 16":'
-jq -c . < graalvm-ce-java16.json
+echo ', "GraalVM CE 17":'
+jq -c . < graalvm-ce-java17.json
 echo ', "GraalVM EE 8":'
 jq -c . < graalvm-ee-java8.json
 echo ', "GraalVM EE 11":'
 jq -c . < graalvm-ee-java11.json
-echo ', "GraalVM EE 16":'
-jq -c . < graalvm-ee-java16.json
+echo ', "GraalVM EE 17":'
+jq -c . < graalvm-ee-java17.json
 echo '}'
