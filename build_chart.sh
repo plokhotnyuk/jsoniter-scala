fb9824f125c6a7f4dd0eb7eb85882b03515f015b
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2022-01-28 06:43:00 +0100

    Update results of JVM benchmarks

--- build_chart.sh
+++ build_chart.sh
@@ -1,4 +1,4 @@
-echo 'var providedBenchmarks = ["Zulu 8", "Zulu 11", "Zulu 17", "GraalVM CE 11", "GraalVM CE 17", "GraalVM EE 8", "GraalVM EE 11", "GraalVM EE 17"];
+echo 'var providedBenchmarks = ["Zulu 8", "Zulu 11", "Zulu 17", "GraalVM CE 11", "GraalVM CE 17", "GraalVM EE 11", "GraalVM EE 17"];
 var providedBenchmarkStore = {
 "Zulu 8":'
 jq -c . < zulu-8.json
@@ -10,8 +10,6 @@ echo ', "GraalVM CE 11":'
 jq -c . < graalvm-ce-java11.json
 echo ', "GraalVM CE 17":'
 jq -c . < graalvm-ce-java17.json
-echo ', "GraalVM EE 8":'
-jq -c . < graalvm-ee-java8.json
 echo ', "GraalVM EE 11":'
 jq -c . < graalvm-ee-java11.json
 echo ', "GraalVM EE 17":'
