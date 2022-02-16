e85b985b621e1dd29b0c226436f014cf6c68277a
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2022-02-16 10:15:44 +0100

    Fix test failures of Core API tests for Scala Native

--- jsoniter-scala-core/js/src/test/scala/com/github/plokhotnyuk/jsoniter_scala/core/TestUtils.scala
+++ jsoniter-scala-core/js/src/test/scala/com/github/plokhotnyuk/jsoniter_scala/core/TestUtils.scala
@@ -2,7 +2,8 @@ package com.github.plokhotnyuk.jsoniter_scala.core
 
 object TestUtils {
   val isJDK8: Boolean = System.getProperty("java.version").startsWith("1.8")
-  val isJS: Boolean = 1.0.toString == "1"
+  val isJS = true
+  val isNative = false
 
   def toHex(b: Byte): String = "%02x".format(b & 0xFF /* FIXME: Scala.js formats bytes as ints */)
 }
