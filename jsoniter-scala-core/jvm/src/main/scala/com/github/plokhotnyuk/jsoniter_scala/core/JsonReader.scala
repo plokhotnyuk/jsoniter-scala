067a2e8ff1cce355c01020408216cbc11b12560d
plokhotnyuk <plokhotnyuk@gmail.com>
2022-04-16 12:42:23 +0200

    More efficient parsing of `java.time.Instant` values for JVMs

--- jsoniter-scala-core/jvm/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/core/JsonReader.scala
+++ jsoniter-scala-core/jvm/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/core/JsonReader.scala
@@ -2022,25 +2022,25 @@ final class JsonReader private[jsoniter_scala](
     val year = parseYearWithByte('-', 10, head)
     val month = parseMonthWithByte('-', head)
     val day = parseDayWithByte(year, month, 'T', head)
-    var seconds = 0L
+    val epochDay = epochDayForYear(year) + (dayOfYearForYearMonth(year, month) + day - 719529) // 719528 == days 0000 to 1970
+    var secondOfDay = 0L
     val pos = head
     if (pos + 7 < tail && {
-      seconds = ByteArrayAccess.getLong(buf, pos)
-      (seconds & 0xF0F0FFF0F0FFF0F0L) == 0x30303A30303A3030L &&
-        ((seconds + 0x060A00060A00060EL) & 0xF0F000F0F000F0F0L) == 0x3030003030003030L
-    } && { // Fast time string to seconds borrowed from here: https://johnnylee-sde.github.io/Fast-time-string-to-seconds/
-      seconds = ((seconds & 0x0F07000F07000F03L) * 2561) >> 8
-      seconds = ((((seconds & 0x3F00001F) * 0xE1000003CL) >> 24) & 0x1FFFF) + (seconds >> 48)
-      seconds < 86400 // 86400 == seconds per day
+      secondOfDay = ByteArrayAccess.getLong(buf, pos)
+      (secondOfDay & 0xF0F0FFF0F0FFF0F0L) == 0x30303A30303A3030L &&
+        ((secondOfDay + 0x060A00060A00060DL) & 0xF0F000F0F000F0F0L) == 0x3030003030003030L
+    } && { // Based on the fast time string to seconds conversion: https://johnnylee-sde.github.io/Fast-time-string-to-seconds/
+      secondOfDay = ((secondOfDay & 0x0F07000F07000F03L) * 2561) >> 8
+      secondOfDay = (((secondOfDay & 0x3F00001F) * 0x70800001e000000L) >>> 47) + (secondOfDay >> 48)
+      secondOfDay < 86400 // 86400 == seconds per day
     }) head = pos + 8
-    else seconds = parseSeconds(pos)
+    else secondOfDay = parseSecondOfDay(pos)
     val nano = parseOptionalNanoWithByte('Z')
     nextByteOrError('"', head)
-    val epochDay = epochDayForYear(year) + (dayOfYearForYearMonth(year, month) + day - 719529) // 719528 == days 0000 to 1970
-    Instant.ofEpochSecond(epochDay * 86400 + seconds, nano)
+    Instant.ofEpochSecond(epochDay * 86400 + secondOfDay, nano)
   }
 
-  private[this] def parseSeconds(pos: Int): Int =
+  private[this] def parseSecondOfDay(pos: Int): Int =
     parseHourWithByte(':', pos) * 3600 + parseMinuteWithByte(':', head) * 60 + parseSecond(head)
 
   private[this] def parseLocalDate(): LocalDate = {
