c57cf3712a2623b9b89614df5f532af296c7b83d
plokhotnyuk <plokhotnyuk@gmail.com>
2022-05-10 16:13:10 +0200

    More efficient serialization of `java.time.ZoneOffset` for JVMs

--- jsoniter-scala-core/jvm/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/core/JsonWriter.scala
+++ jsoniter-scala-core/jvm/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/core/JsonWriter.scala
@@ -1411,29 +1411,26 @@ final class JsonWriter private[jsoniter_scala](
       pos + 3
     } else {
       val ds = digits
-      var m = 0x3A00002B22L
+      var m = 0x2230303A00002B22L
       if (q0 < 0) {
         q0 = -q0
-        m = 0x3A00002D22L
+        m = 0x2230303A00002D22L
       }
       val p1 = q0 * 37283
       val q1 = p1 >>> 27 // divide a small positive int by 3600
       m |= ds(q1) << 16
       if ((p1 & 0x7FF8000) == 0) { // check if q0 is divisible by 3600
-        ByteArrayAccess.setLong(buf, pos, m | 0x2230300000000000L)
+        ByteArrayAccess.setLong(buf, pos, m)
         pos + 8
       } else {
         val r1 = q0 - q1 * 3600
         val p2 = r1 * 17477
         val q2 = p2 >> 20 // divide a small positive int by 60
         m |= ds(q2).toLong << 40
-        if ((p2 & 0xFC000) == 0) { // check if r1 is divisible by 60
-          ByteArrayAccess.setLong(buf, pos, m | 0x2200000000000000L)
-          pos + 8
-        } else {
-          val r2 = r1 - q2 * 60
-          ByteArrayAccess.setLong(buf, pos, m | 0x3A00000000000000L)
-          ByteArrayAccess.setInt(buf, pos + 8, ds(r2) | 0x220000)
+        ByteArrayAccess.setLong(buf, pos, m)
+        if ((p2 & 0xFC000) == 0) pos + 8 // check if r1 is divisible by 60
+        else {
+          ByteArrayAccess.setInt(buf, pos + 7, ds(r1 - q2 * 60) << 8 | 0x2200003A)
           pos + 11
         }
       }
