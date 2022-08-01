509b16d35ad058bbb37d7f7b65818a016f542abe
plokhotnyuk <plokhotnyuk@gmail.com>
2022-08-01 12:04:28 +0200

    More efficient serialization of 1-char strings with the circe booster

--- jsoniter-scala-circe/shared/src/main/scala/io/circe/JsoniterScalaCodec.scala
+++ jsoniter-scala-circe/shared/src/main/scala/io/circe/JsoniterScalaCodec.scala
@@ -114,7 +114,10 @@ final class JsoniterScalaCodec(
   }
 
   private[this] def encode(x: Json, out: JsonWriter, depth: Int): Unit = x match {
-    case s: JString => out.writeVal(s.value)
+    case s: JString =>
+      val str = s.value
+      if (str.length == 1) out.writeVal(str.charAt(0))
+      else out.writeVal(s.value)
     case b: JBoolean => out.writeVal(b.value)
     case n: JNumber => encodeJsonNumber(n.value, out)
     case a: JArray =>
