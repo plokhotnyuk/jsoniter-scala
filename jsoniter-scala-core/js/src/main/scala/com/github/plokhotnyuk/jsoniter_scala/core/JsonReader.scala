1d5f85c5d3b7a81af196c713c010a327908cdbe3
plokhotnyuk <plokhotnyuk@gmail.com>
2022-08-24 10:00:25 +0200

    Fix an unexpected `java.lang.NegativeArraySizeException` exception when parsing from `InputStream` or `DirectByteBuffer` using an internal buffer that exceed 1Gb

--- jsoniter-scala-core/js/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/core/JsonReader.scala
+++ jsoniter-scala-core/js/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/core/JsonReader.scala
@@ -2986,7 +2986,13 @@ final class JsonReader private[jsoniter_scala](
       if (mark > 0) mark = 0
       tail = remaining
       head = newPos
-    } else buf = java.util.Arrays.copyOf(buf, buf.length << 1)
+    } else {
+      var bufLen = buf.length
+      if (bufLen < 1073741824) bufLen <<= 1
+      else if (bufLen < 2147483647) bufLen = 2147483647
+      else tooLongInputError()
+      buf = java.util.Arrays.copyOf(buf, bufLen)
+    }
     var len = buf.length - tail
     if (bbuf ne null) {
       len = Math.min(bbuf.remaining, len)
@@ -2997,6 +3003,8 @@ final class JsonReader private[jsoniter_scala](
     totalRead += len
     newPos
   }
+
+  private[this] def tooLongInputError(): Nothing = decodeError("too long input", tail)
 
   private[this] def endOfInputError(): Nothing = decodeError("unexpected end of input", tail)
 
