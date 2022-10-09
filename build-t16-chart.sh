7dada5abb4932fa252229f259b5da6301f2725b2
plokhotnyuk <plokhotnyuk@gmail.com>
2022-10-09 14:52:44 +0200

    Add benchmark results for 16 threads from JVMs

---
+++ build-t16-chart.sh
@@ -0,0 +1,19 @@
+echo 'var providedBenchmarks = ["Zulu 11", "Zulu 17", "OpenJDK 20", "GraalVM CE 11", "GraalVM CE 17", "GraalVM CE 19", "GraalVM EE 11", "GraalVM EE 17"];
+var providedBenchmarkStore = {
+"Zulu 11":'
+jq -c . < zulu-11-t16.json
+echo ', "Zulu 17":'
+jq -c . < zulu-17-t16.json
+echo ', "OpenJDK 20":'
+jq -c . < openjdk-20-t16.json
+echo ', "GraalVM CE 11":'
+jq -c . < graalvm-ce-java11-t16.json
+echo ', "GraalVM CE 17":'
+jq -c . < graalvm-ce-java17-t16.json
+echo ', "GraalVM CE 19":'
+jq -c . < graalvm-ce-java19-t16.json
+echo ', "GraalVM EE 11":'
+jq -c . < graalvm-ee-java11-t16.json
+echo ', "GraalVM EE 17":'
+jq -c . < graalvm-ee-java17-t16.json
+echo '}'
