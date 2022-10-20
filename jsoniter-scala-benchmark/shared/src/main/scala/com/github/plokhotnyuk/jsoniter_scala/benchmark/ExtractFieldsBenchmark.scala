5480a829837a4d58affdca180025055340afbeea
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2022-10-20 15:00:32 +0200

    Add commented out inputs for testing of different vulnerabilities

--- jsoniter-scala-benchmark/shared/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/benchmark/ExtractFieldsBenchmark.scala
+++ jsoniter-scala-benchmark/shared/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/benchmark/ExtractFieldsBenchmark.scala
@@ -16,7 +16,10 @@ abstract class ExtractFieldsBenchmark extends CommonParams {
   def setup(): Unit = {
     val value = """{"number":0.0,"boolean":false,"string":null}"""
     jsonString = zeroHashCodeStrings.map(s => writeToString(s)(JsoniterScalaCodecs.stringCodec)).take(size)
-      .mkString("""{"s":"s",""", s""":$value,""", s""":$value,"i":1}""")
+      .mkString("""{"s":"s",""", s""":$value,""", s""":$value,"i":1}""") // test for hashcode collision vulnerability
+    //jsonString = s"""{"s":"s","n":${"7" * size},"i":1}""" // test for big number parsing vulnerability
+    //jsonString = s"""{"s":"s","n":${"[" * size + "]" * size},"i":1}""" // test for nested JSON array vulnerability
+    //jsonString = s"""{"s":"s",${""""n":{""" * size + "}" * size},"i":1}""" // test for nested JSON object vulnerability
     jsonBytes = jsonString.getBytes(UTF_8)
   }
 }
