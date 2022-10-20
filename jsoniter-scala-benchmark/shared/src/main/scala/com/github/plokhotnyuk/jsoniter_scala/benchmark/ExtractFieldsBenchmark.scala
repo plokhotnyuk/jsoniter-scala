ed5e47c9074998db753fc9a2f248347d75070836
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2022-10-20 17:10:41 +0200

    Reproduce DoS parsing a JSON object with an unexpected field that has a big number using weePickle and jackson-module-scala

--- jsoniter-scala-benchmark/shared/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/benchmark/ExtractFieldsBenchmark.scala
+++ jsoniter-scala-benchmark/shared/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/benchmark/ExtractFieldsBenchmark.scala
@@ -15,9 +15,9 @@ abstract class ExtractFieldsBenchmark extends CommonParams {
   @Setup
   def setup(): Unit = {
     val value = """{"number":0.0,"boolean":false,"string":null}"""
-    jsonString = zeroHashCodeStrings.map(s => writeToString(s)(JsoniterScalaCodecs.stringCodec)).take(size)
-      .mkString("""{"s":"s",""", s""":$value,""", s""":$value,"i":1}""") // test for hashcode collision vulnerability
-    //jsonString = s"""{"s":"s","n":${"7" * size},"i":1}""" // test for big number parsing vulnerability
+    //jsonString = zeroHashCodeStrings.map(s => writeToString(s)(JsoniterScalaCodecs.stringCodec)).take(size)
+      //.mkString("""{"s":"s",""", s""":$value,""", s""":$value,"i":1}""") // test for hashcode collision vulnerability
+    jsonString = s"""{"s":"s","n":${"7" * size},"i":1}""" // test for big number parsing vulnerability
     //jsonString = s"""{"s":"s","n":${"[" * size + "]" * size},"i":1}""" // test for nested JSON array vulnerability
     //jsonString = s"""{"s":"s",${""""n":{""" * size + "}" * size},"i":1}""" // test for nested JSON object vulnerability
     jsonBytes = jsonString.getBytes(UTF_8)
