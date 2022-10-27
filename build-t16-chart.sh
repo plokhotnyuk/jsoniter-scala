6f42a3e0bf5709d99abe07a3b2cb87cdcc377e96
plokhotnyuk <plokhotnyuk@gmail.com>
2022-10-27 17:08:56 +0200

    Update benchmark results from JVMs for v2.17.6

--- build-t16-chart.sh
+++ build-t16-chart.sh
@@ -1,4 +1,4 @@
-echo 'var providedBenchmarks = ["Zulu 11", "Zulu 17", "OpenJDK 20", "GraalVM CE 11", "GraalVM CE 17", "GraalVM CE 19", "GraalVM EE 11", "GraalVM EE 17"];
+echo 'var providedBenchmarks = ["Zulu 11", "Zulu 17", "OpenJDK 20", "GraalVM CE 11", "GraalVM CE 17", "GraalVM CE 19", "GraalVM EE 11", "GraalVM EE 17", "GraalVM EE 19"];
 var providedBenchmarkStore = {
 "Zulu 11":'
 jq -c . < zulu-11-t16.json
@@ -16,4 +16,6 @@ echo ', "GraalVM EE 11":'
 jq -c . < graalvm-ee-java11-t16.json
 echo ', "GraalVM EE 17":'
 jq -c . < graalvm-ee-java17-t16.json
+echo ', "GraalVM EE 19":'
+jq -c . < graalvm-ee-java19-t16.json
 echo '}'
