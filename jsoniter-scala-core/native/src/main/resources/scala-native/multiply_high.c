31a30e44f4aa19f6e8a1a816c062caf9dd3923a7
Arman Bilge <armanbilge@gmail.com>
2022-11-08 01:45:06 -0800

    Add `jsoniter_scala_unsigned_multiply_high`

--- jsoniter-scala-core/native/src/main/resources/scala-native/multiply_high.c
+++ jsoniter-scala-core/native/src/main/resources/scala-native/multiply_high.c
@@ -2,3 +2,8 @@ long jsoniter_scala_multiply_high(long x, long y) {
   unsigned __int128 z = x * (unsigned __int128)y;
   return z >> 64;
 }
+
+unsigned long jsoniter_scala_unsigned_multiply_high(unsigned long x, unsigned long y) {
+  unsigned __int128 z = x * (unsigned __int128)y;
+  return z >> 64;
+}
