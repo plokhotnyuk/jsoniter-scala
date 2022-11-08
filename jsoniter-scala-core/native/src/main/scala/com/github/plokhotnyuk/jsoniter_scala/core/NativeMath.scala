f8d0c3276b3a6ea96243eb4884ff366589edee2f
Arman Bilge <armanbilge@gmail.com>
2022-11-08 01:45:54 -0800

    Add `unsignedMultiplyHigh`

--- jsoniter-scala-core/native/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/core/NativeMath.scala
+++ jsoniter-scala-core/native/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/core/NativeMath.scala
@@ -8,4 +8,7 @@ object NativeMath {
   @name("jsoniter_scala_multiply_high")
   def multiplyHigh(x: CLong, y: CLong): CLong = extern
 
+  @name("jsoniter_scala_unsigned_multiply_high")
+  def unsignedMultiplyHigh(x: CLong, y: CLong): CLong = extern
+
 }
