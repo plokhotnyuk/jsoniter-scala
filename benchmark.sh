c4a91a68950e8c06459af10da8c90bbff442f5ab
plokhotnyuk <plokhotnyuk@gmail.com>
2022-12-11 16:51:17 +0100

    Update benchmark scripts for OpenJDK-21

--- benchmark.sh
+++ benchmark.sh
@@ -4,6 +4,6 @@ sbt -batch -java-home /usr/lib/jvm/graalvm-ee-java17 clean 'jsoniter-scala-bench
 sbt -batch -java-home /usr/lib/jvm/graalvm-ee-java11 clean 'jsoniter-scala-benchmarkJVM/jmh:run -p size=128 -prof gc -rf json -rff graalvm-ee-java11.json .*' 2>&1 | tee graalvm-ee-java11.txt
 sbt -batch -java-home /usr/lib/jvm/graalvm-ce-java19 clean 'jsoniter-scala-benchmarkJVM/jmh:run -p size=128 -prof gc -rf json -rff graalvm-ce-java19.json .*' 2>&1 | tee graalvm-ce-java19.txt
 sbt -batch -java-home /usr/lib/jvm/graalvm-ce-java17 clean 'jsoniter-scala-benchmarkJVM/jmh:run -p size=128 -prof gc -rf json -rff graalvm-ce-java17.json .*' 2>&1 | tee graalvm-ce-java17.txt
-sbt -batch -java-home /usr/lib/jvm/openjdk-20 clean 'jsoniter-scala-benchmarkJVM/jmh:run -p size=128 -prof gc -rf json -rff openjdk-20.json .*' 2>&1 | tee openjdk-20.txt
+sbt -batch -java-home /usr/lib/jvm/openjdk-21 clean 'jsoniter-scala-benchmarkJVM/jmh:run -p size=128 -prof gc -rf json -rff openjdk-21.json .*' 2>&1 | tee openjdk-21.txt
 sbt -batch -java-home /usr/lib/jvm/zulu-17 clean 'jsoniter-scala-benchmarkJVM/jmh:run -p size=128 -prof gc -rf json -rff zulu-17.json .*' 2>&1 | tee zulu-17.txt
 sbt -batch -java-home /usr/lib/jvm/zulu-11 clean 'jsoniter-scala-benchmarkJVM/jmh:run -p size=128 -prof gc -rf json -rff zulu-11.json .*' 2>&1 | tee zulu-11.txt
