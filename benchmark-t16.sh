de928c620501f810a144a413fa36d2e33875993c
plokhotnyuk <plokhotnyuk@gmail.com>
2023-02-21 08:41:30 +0100

    Update GraalVM CE for the latest dev version that is based on JDK 20

--- benchmark-t16.sh
+++ benchmark-t16.sh
@@ -2,7 +2,7 @@
 sbt -batch -java-home /usr/lib/jvm/graalvm-ee-java19 clean 'jsoniter-scala-benchmarkJVM/jmh:run -t 16 -p size=128 -prof gc -rf json -rff graalvm-ee-java19-t16.json .*' 2>&1 | tee graalvm-ee-java19-t16.txt
 sbt -batch -java-home /usr/lib/jvm/graalvm-ee-java17 clean 'jsoniter-scala-benchmarkJVM/jmh:run -t 16 -p size=128 -prof gc -rf json -rff graalvm-ee-java17-t16.json .*' 2>&1 | tee graalvm-ee-java17-t16.txt
 sbt -batch -java-home /usr/lib/jvm/graalvm-ee-java11 clean 'jsoniter-scala-benchmarkJVM/jmh:run -t 16 -p size=128 -prof gc -rf json -rff graalvm-ee-java11-t16.json .*' 2>&1 | tee graalvm-ee-java11-t16.txt
-sbt -batch -java-home /usr/lib/jvm/graalvm-ce-java19 clean 'jsoniter-scala-benchmarkJVM/jmh:run -t 16 -p size=128 -prof gc -rf json -rff graalvm-ce-java19-t16.json .*' 2>&1 | tee graalvm-ce-java19-t16.txt
+sbt -batch -java-home /usr/lib/jvm/graalvm-ce-java20 clean 'jsoniter-scala-benchmarkJVM/jmh:run -t 16 -p size=128 -prof gc -rf json -rff graalvm-ce-java20-t16.json .*' 2>&1 | tee graalvm-ce-java20-t16.txt
 sbt -batch -java-home /usr/lib/jvm/openjdk-21 clean 'jsoniter-scala-benchmarkJVM/jmh:run -t 16 -p size=128 -prof gc -rf json -rff openjdk-21-t16.json .*' 2>&1 | tee openjdk-21-t16.txt
 sbt -batch -java-home /usr/lib/jvm/zulu-17 clean 'jsoniter-scala-benchmarkJVM/jmh:run -t 16 -p size=128 -prof gc -rf json -rff zulu-17-t16.json .*' 2>&1 | tee zulu-17-t16.txt
 sbt -batch -java-home /usr/lib/jvm/zulu-11 clean 'jsoniter-scala-benchmarkJVM/jmh:run -t 16 -p size=128 -prof gc -rf json -rff zulu-11-t16.json .*' 2>&1 | tee zulu-11-t16.txt
