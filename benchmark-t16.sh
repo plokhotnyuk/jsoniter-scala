e53dcde764911369fe4dc209c6c62991b8515e60
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2023-04-30 03:48:27 +0200

    GraalVM EE has no release for Java 19

--- benchmark-t16.sh
+++ benchmark-t16.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sbt -batch -java-home /usr/lib/jvm/graalvm-ee-java19 clean 'jsoniter-scala-benchmarkJVM/jmh:run -t 16 -p size=128 -prof gc -rf json -rff graalvm-ee-java19-t16.json .*' 2>&1 | tee graalvm-ee-java19-t16.txt
 sbt -batch -java-home /usr/lib/jvm/graalvm-ee-java17 clean 'jsoniter-scala-benchmarkJVM/jmh:run -t 16 -p size=128 -prof gc -rf json -rff graalvm-ee-java17-t16.json .*' 2>&1 | tee graalvm-ee-java17-t16.txt
 sbt -batch -java-home /usr/lib/jvm/graalvm-ee-java11 clean 'jsoniter-scala-benchmarkJVM/jmh:run -t 16 -p size=128 -prof gc -rf json -rff graalvm-ee-java11-t16.json .*' 2>&1 | tee graalvm-ee-java11-t16.txt
 sbt -batch -java-home /usr/lib/jvm/graalvm-ce-java20 clean 'jsoniter-scala-benchmarkJVM/jmh:run -t 16 -p size=128 -prof gc -rf json -rff graalvm-ce-java20-t16.json .*' 2>&1 | tee graalvm-ce-java20-t16.txt
--- benchmark.sh
+++ benchmark.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sbt -batch -java-home /usr/lib/jvm/graalvm-ee-java19 clean 'jsoniter-scala-benchmarkJVM/jmh:run -p size=128 -prof gc -rf json -rff graalvm-ee-java19.json .*' 2>&1 | tee graalvm-ee-java19.txt
 sbt -batch -java-home /usr/lib/jvm/graalvm-ee-java17 clean 'jsoniter-scala-benchmarkJVM/jmh:run -p size=128 -prof gc -rf json -rff graalvm-ee-java17.json .*' 2>&1 | tee graalvm-ee-java17.txt
 sbt -batch -java-home /usr/lib/jvm/graalvm-ee-java11 clean 'jsoniter-scala-benchmarkJVM/jmh:run -p size=128 -prof gc -rf json -rff graalvm-ee-java11.json .*' 2>&1 | tee graalvm-ee-java11.txt
 sbt -batch -java-home /usr/lib/jvm/graalvm-ce-java20 clean 'jsoniter-scala-benchmarkJVM/jmh:run -p size=128 -prof gc -rf json -rff graalvm-ce-java20.json .*' 2>&1 | tee graalvm-ce-java20.txt
