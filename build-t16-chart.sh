5c62d1786b7b07f2cf4d625d96764e739607f548
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2023-04-30 03:52:35 +0200

    Update benchmark results for JVMs using 1 thread

--- build-t16-chart.sh
+++ build-t16-chart.sh
@@ -1,4 +1,4 @@
-echo 'var providedBenchmarks = ["Zulu 11", "Zulu 17", "OpenJDK 21", "GraalVM CE 17", "GraalVM CE 20", "GraalVM EE 11", "GraalVM EE 17", "GraalVM EE 19"];
+echo 'var providedBenchmarks = ["Zulu 11", "Zulu 17", "OpenJDK 21", "GraalVM CE 17", "GraalVM CE 20", "GraalVM EE 11", "GraalVM EE 17"];
 var providedBenchmarkStore = {
 "Zulu 11":'
 jq -c . < zulu-11-t16.json
@@ -14,6 +14,4 @@ echo ', "GraalVM EE 11":'
 jq -c . < graalvm-ee-java11-t16.json
 echo ', "GraalVM EE 17":'
 jq -c . < graalvm-ee-java17-t16.json
-echo ', "GraalVM EE 19":'
-jq -c . < graalvm-ee-java19-t16.json
 echo '}'
