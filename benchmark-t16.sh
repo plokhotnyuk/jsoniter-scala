3fdf38286f58c4a48d85b458c30424f356d70f6f
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2023-08-02 09:13:27 +0200

    Update GraalVM Community to JDK 21

--- benchmark-t16.sh
+++ benchmark-t16.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sbt -batch -java-home /usr/lib/jvm/oracle-graalvm-jdk-20 clean 'jsoniter-scala-benchmarkJVM/jmh:run -t 16 -p size=128 -prof gc -rf json -rff oracle-graalvm-jdk-20-t16.json .*' 2>&1 | tee oracle-graalvm-jdk-20-t16.txt
 sbt -batch -java-home /usr/lib/jvm/oracle-graalvm-jdk-17 clean 'jsoniter-scala-benchmarkJVM/jmh:run -t 16 -p size=128 -prof gc -rf json -rff oracle-graalvm-jdk-17-t16.json .*' 2>&1 | tee oracle-graalvm-jdk-17-t16.txt
-sbt -batch -java-home /usr/lib/jvm/graalvm-community-jdk-20 clean 'jsoniter-scala-benchmarkJVM/jmh:run -t 16 -p size=128 -prof gc -rf json -rff graalvm-community-jdk-20-t16.json .*' 2>&1 | tee graalvm-community-jdk-20-t16.txt
+sbt -batch -java-home /usr/lib/jvm/graalvm-community-jdk-17 clean 'jsoniter-scala-benchmarkJVM/jmh:run -jvm /usr/lib/jvm/graalvm-community-jdk-21/bin/java -t 16 -p size=128 -prof gc -rf json -rff graalvm-community-jdk-21-t16.json .*' 2>&1 | tee graalvm-community-jdk-21-t16.txt
 sbt -batch -java-home /usr/lib/jvm/graalvm-community-jdk-17 clean 'jsoniter-scala-benchmarkJVM/jmh:run -t 16 -p size=128 -prof gc -rf json -rff graalvm-community-jdk-17-t16.json .*' 2>&1 | tee graalvm-community-jdk-17-t16.txt
 sbt -batch -java-home /usr/lib/jvm/zulu-17 clean 'jsoniter-scala-benchmarkJVM/jmh:run -jvm /usr/lib/jvm/jdk-22/bin/java -t 16 -p size=128 -prof gc -rf json -rff jdk-22-t16.json .*' 2>&1 | tee jdk-22-t16.txt
 sbt -batch -java-home /usr/lib/jvm/zulu-17 clean 'jsoniter-scala-benchmarkJVM/jmh:run -t 16 -p size=128 -prof gc -rf json -rff zulu-17-t16.json .*' 2>&1 | tee zulu-17-t16.txt
--- benchmark.sh
+++ benchmark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sbt -batch -java-home /usr/lib/jvm/oracle-graalvm-jdk-20 clean 'jsoniter-scala-benchmarkJVM/jmh:run -p size=128 -prof gc -rf json -rff oracle-graalvm-jdk-20.json .*' 2>&1 | tee oracle-graalvm-jdk-20.txt
 sbt -batch -java-home /usr/lib/jvm/oracle-graalvm-jdk-17 clean 'jsoniter-scala-benchmarkJVM/jmh:run -p size=128 -prof gc -rf json -rff oracle-graalvm-jdk-17.json .*' 2>&1 | tee oracle-graalvm-jdk-17.txt
-sbt -batch -java-home /usr/lib/jvm/graalvm-community-jdk-20 clean 'jsoniter-scala-benchmarkJVM/jmh:run -p size=128 -prof gc -rf json -rff graalvm-community-jdk-20.json .*' 2>&1 | tee graalvm-community-jdk-20.txt
+sbt -batch -java-home /usr/lib/jvm/graalvm-community-jdk-17 clean 'jsoniter-scala-benchmarkJVM/jmh:run -jvm /usr/lib/jvm/graalvm-community-jdk-21/bin/java -p size=128 -prof gc -rf json -rff graalvm-community-jdk-21.json .*' 2>&1 | tee graalvm-community-jdk-21.txt
 sbt -batch -java-home /usr/lib/jvm/graalvm-community-jdk-17 clean 'jsoniter-scala-benchmarkJVM/jmh:run -p size=128 -prof gc -rf json -rff graalvm-community-jdk-17.json .*' 2>&1 | tee graalvm-community-jdk-17.txt
 sbt -batch -java-home /usr/lib/jvm/zulu-17 clean 'jsoniter-scala-benchmarkJVM/jmh:run -jvm /usr/lib/jvm/jdk-22/bin/java -p size=128 -prof gc -rf json -rff jdk-22.json .*' 2>&1 | tee jdk-22.txt
 sbt -batch -java-home /usr/lib/jvm/zulu-17 clean 'jsoniter-scala-benchmarkJVM/jmh:run -p size=128 -prof gc -rf json -rff zulu-17.json .*' 2>&1 | tee zulu-17.txt
