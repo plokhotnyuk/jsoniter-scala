1d36456cd080add755fee4be784d4e917b631142
Andriy Plokhotnyuk <andriy.plokhotnyuk@superfund-technologies.com>
2023-08-16 08:36:16 +0200

    Update results from JVMs after upgrade of Intel microcode

--- build-t16-chart.sh
+++ build-t16-chart.sh
@@ -1,4 +1,4 @@
-echo 'var providedBenchmarks = ["Zulu 11", "Zulu 17", "OpenJDK 22", "GraalVM Community 17", "GraalVM Community 21", "Oracle GraalVM 17", "Oracle GraalVM 20"];
+echo 'var providedBenchmarks = ["Zulu 11", "Zulu 17", "OpenJDK 22", "GraalVM Community 21", "Oracle GraalVM 17", "Oracle GraalVM 20"];
 var providedBenchmarkStore = {
 "Zulu 11":'
 jq -c . < zulu-11-t16.json
@@ -6,8 +6,6 @@ echo ', "Zulu 17":'
 jq -c . < zulu-17-t16.json
 echo ', "OpenJDK 22":'
 jq -c . < jdk-22-t16.json
-echo ', "GraalVM Community 17":'
-jq -c . < graalvm-community-jdk-17-t16.json
 echo ', "GraalVM Community 21":'
 jq -c . < graalvm-community-jdk-21-t16.json
 echo ', "Oracle GraalVM 17":'
