2bd76dc279c9190918ef0eb9abe22c79a3e21d12
plokhotnyuk <plokhotnyuk@gmail.com>
2024-01-24 10:53:14 +0100

    Update results from JVMs

--- build-chart.sh
+++ build-chart.sh
@@ -1,5 +1,5 @@
 sed -i '/"gc./ s//"·gc./g' *.json
-echo 'var providedBenchmarks = ["JDK 17", "JDK 21", "JDK 23", "GraalVM Community JDK 17", "GraalVM Community JDK 21", "GraalVM JDK 17", "GraalVM JDK 21"];
+echo 'var providedBenchmarks = ["JDK 17", "JDK 21", "JDK 23", "GraalVM Community JDK 21", "GraalVM JDK 17", "GraalVM JDK 21"];
 var providedBenchmarkStore = {
 "JDK 17":'
 jq -c . < jdk-17.json
@@ -7,8 +7,6 @@ echo ', "JDK 21":'
 jq -c . < jdk-21.json
 echo ', "JDK 23":'
 jq -c . < jdk-23.json
-echo ', "GraalVM Community JDK 17":'
-jq -c . < graalvm-community-jdk-17.json
 echo ', "GraalVM Community JDK 21":'
 jq -c . < graalvm-community-jdk-21.json
 echo ', "GraalVM JDK 17":'
