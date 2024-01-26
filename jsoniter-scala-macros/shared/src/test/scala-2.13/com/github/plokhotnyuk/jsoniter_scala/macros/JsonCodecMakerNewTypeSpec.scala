a175d2562309c2ddd3e8c46cafa0dea0e7dcd8bd
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2024-01-26 14:48:47 +0100

    Add missing tests for `mutable.ArrayDeque` and `mutable.PriorityQueue` collections

--- jsoniter-scala-macros/shared/src/test/scala-2.13/com/github/plokhotnyuk/jsoniter_scala/macros/JsonCodecMakerNewTypeSpec.scala
+++ jsoniter-scala-macros/shared/src/test/scala-2.13/com/github/plokhotnyuk/jsoniter_scala/macros/JsonCodecMakerNewTypeSpec.scala
@@ -16,6 +16,12 @@ class JsonCodecMakerNewTypeSpec extends VerifyingSpec {
       verifySerDeser(make[_root_.scala.collection.immutable.VectorMap[String, Int]],
         _root_.scala.collection.immutable.VectorMap[String, Int]("WWW" -> 1, "VVV" -> 2),
         """{"WWW":1,"VVV":2}""")
+      verifySerDeser(make[_root_.scala.collection.mutable.ArrayDeque[Int]],
+        _root_.scala.collection.mutable.ArrayDeque[Int](1, 2), """[1,2]""")
+      verifySer(make[_root_.scala.collection.mutable.PriorityQueue[Int]],
+        _root_.scala.collection.mutable.PriorityQueue[Int](2, 1), """[2,1]""")
+      verifyDeserByCheck(make[_root_.scala.collection.mutable.PriorityQueue[Int]],
+        """[2,1]""", (x: _root_.scala.collection.mutable.PriorityQueue[Int]) => x.toList shouldBe List(2, 1))
     }
   }
 }
