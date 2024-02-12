4dfb981c698d29aa1ef857cebcb5ac738fd43ddd
plokhotnyuk <plokhotnyuk@gmail.com>
2024-02-12 10:16:40 +0100

    Update benchmark results from JVMs

--- build-chart.sh
+++ build-chart.sh
@@ -1,5 +1,5 @@
 sed -i '/"gc./ s//"·gc./g' *.json
-echo 'var providedBenchmarks = ["JDK 17", "JDK 21", "JDK 23", "GraalVM CE JDK 21", "GraalVM CE JDK 22", "GraalVM JDK 17", "GraalVM JDK 21"];
+echo 'var providedBenchmarks = ["JDK 17", "JDK 21", "JDK 23", "GraalVM CE JDK 21", "GraalVM CE JDK 23", "GraalVM JDK 17", "GraalVM JDK 21"];
 var providedBenchmarkStore = {
 "JDK 17":'
 jq -c . < jdk-17.json
@@ -9,8 +9,8 @@ echo ', "JDK 23":'
 jq -c . < jdk-23.json
 echo ', "GraalVM CE JDK 21":'
 jq -c . < graalvm-community-jdk-21.json
-echo ', "GraalVM CE JDK 22":'
-jq -c . < graalvm-community-jdk-22.json
+echo ', "GraalVM CE JDK 23":'
+jq -c . < graalvm-community-jdk-23.json
 echo ', "GraalVM JDK 17":'
 jq -c . < graalvm-jdk-17.json
 echo ', "GraalVM JDK 21":'
