58f2ef44fe6549bfeff60160d5094f2282d640ac
plokhotnyuk <plokhotnyuk@gmail.com>
2024-03-19 15:31:58 +0100

    Add GraalVM JDK 22 to benchmark scripts

--- benchmark-t16.sh
+++ benchmark-t16.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-sbt -batch -java-home /usr/lib/jvm/graalvm-community-jdk-23 clean 'jsoniter-scala-benchmarkJVM/jmh:run -jvmArgsAppend "-Djmh.executor=FJP" -t 16 -p size=128 -prof gc -rf json -rff graalvm-community-jdk-23-t16.json .*' 2>&1 | tee graalvm-community-jdk-23-t16.txt
-sbt -batch -java-home /usr/lib/jvm/jdk-23 clean 'jsoniter-scala-benchmarkJVM/jmh:run -jvmArgsAppend "-Djmh.executor=FJP" -t 16 -p size=128 -prof gc -rf json -rff jdk-23-t16.json .*' 2>&1 | tee jdk-23-t16.txt
+sbt -batch -java-home /usr/lib/jvm/graalvm-jdk-22 clean 'jsoniter-scala-benchmarkJVM/jmh:run -jvmArgsAppend "-Djmh.executor=FJP" -t 16 -p size=128 -prof gc -rf json -rff graalvm-jdk-22-t16.json .*' 2>&1 | tee graalvm-jdk-22-t16.txt
 sbt -batch -java-home /usr/lib/jvm/graalvm-jdk-21 clean 'jsoniter-scala-benchmarkJVM/jmh:run -jvmArgsAppend "-Djmh.executor=FJP" -t 16 -p size=128 -prof gc -rf json -rff graalvm-jdk-21-t16.json .*' 2>&1 | tee graalvm-jdk-21-t16.txt
 sbt -batch -java-home /usr/lib/jvm/graalvm-jdk-17 clean 'jsoniter-scala-benchmarkJVM/jmh:run -jvmArgsAppend "-Djmh.executor=FJP" -t 16 -p size=128 -prof gc -rf json -rff graalvm-jdk-17-t16.json .*' 2>&1 | tee graalvm-jdk-17-t16.txt
+sbt -batch -java-home /usr/lib/jvm/graalvm-community-jdk-23 clean 'jsoniter-scala-benchmarkJVM/jmh:run -jvmArgsAppend "-Djmh.executor=FJP" -t 16 -p size=128 -prof gc -rf json -rff graalvm-community-jdk-23-t16.json .*' 2>&1 | tee graalvm-community-jdk-23-t16.txt
 sbt -batch -java-home /usr/lib/jvm/graalvm-community-jdk-21 clean 'jsoniter-scala-benchmarkJVM/jmh:run -jvmArgsAppend "-Djmh.executor=FJP" -t 16 -p size=128 -prof gc -rf json -rff graalvm-community-jdk-21-t16.json .*' 2>&1 | tee graalvm-community-jdk-21-t16.txt
+sbt -batch -java-home /usr/lib/jvm/jdk-23 clean 'jsoniter-scala-benchmarkJVM/jmh:run -jvmArgsAppend "-Djmh.executor=FJP" -t 16 -p size=128 -prof gc -rf json -rff jdk-23-t16.json .*' 2>&1 | tee jdk-23-t16.txt
 sbt -batch -java-home /usr/lib/jvm/jdk-21 clean 'jsoniter-scala-benchmarkJVM/jmh:run -jvmArgsAppend "-Djmh.executor=FJP" -t 16 -p size=128 -prof gc -rf json -rff jdk-21-t16.json .*' 2>&1 | tee jdk-21-t16.txt
 sbt -batch -java-home /usr/lib/jvm/jdk-17 clean 'jsoniter-scala-benchmarkJVM/jmh:run -jvmArgsAppend "-Djmh.executor=FJP" -t 16 -p size=128 -prof gc -rf json -rff jdk-17-t16.json .*' 2>&1 | tee jdk-17-t16.txt
--- benchmark.sh
+++ benchmark.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-sbt -batch -java-home /usr/lib/jvm/graalvm-community-jdk-23 clean 'jsoniter-scala-benchmarkJVM/jmh:run -jvmArgsAppend "-Djmh.executor=FJP" -p size=128 -prof gc -rf json -rff graalvm-community-jdk-23.json .*' 2>&1 | tee graalvm-community-jdk-23.txt
-sbt -batch -java-home /usr/lib/jvm/jdk-23 clean 'jsoniter-scala-benchmarkJVM/jmh:run -jvmArgsAppend "-Djmh.executor=FJP" -p size=128 -prof gc -rf json -rff jdk-23.json .*' 2>&1 | tee jdk-23.txt
+sbt -batch -java-home /usr/lib/jvm/graalvm-jdk-22 clean 'jsoniter-scala-benchmarkJVM/jmh:run -jvmArgsAppend "-Djmh.executor=FJP" -p size=128 -prof gc -rf json -rff graalvm-jdk-22.json .*' 2>&1 | tee graalvm-jdk-22.txt
 sbt -batch -java-home /usr/lib/jvm/graalvm-jdk-21 clean 'jsoniter-scala-benchmarkJVM/jmh:run -jvmArgsAppend "-Djmh.executor=FJP" -p size=128 -prof gc -rf json -rff graalvm-jdk-21.json .*' 2>&1 | tee graalvm-jdk-21.txt
 sbt -batch -java-home /usr/lib/jvm/graalvm-jdk-17 clean 'jsoniter-scala-benchmarkJVM/jmh:run -jvmArgsAppend "-Djmh.executor=FJP" -p size=128 -prof gc -rf json -rff graalvm-jdk-17.json .*' 2>&1 | tee graalvm-jdk-17.txt
+sbt -batch -java-home /usr/lib/jvm/graalvm-community-jdk-23 clean 'jsoniter-scala-benchmarkJVM/jmh:run -jvmArgsAppend "-Djmh.executor=FJP" -p size=128 -prof gc -rf json -rff graalvm-community-jdk-23.json .*' 2>&1 | tee graalvm-community-jdk-23.txt
 sbt -batch -java-home /usr/lib/jvm/graalvm-community-jdk-21 clean 'jsoniter-scala-benchmarkJVM/jmh:run -jvmArgsAppend "-Djmh.executor=FJP" -p size=128 -prof gc -rf json -rff graalvm-community-jdk-21.json .*' 2>&1 | tee graalvm-community-jdk-21.txt
+sbt -batch -java-home /usr/lib/jvm/jdk-23 clean 'jsoniter-scala-benchmarkJVM/jmh:run -jvmArgsAppend "-Djmh.executor=FJP" -p size=128 -prof gc -rf json -rff jdk-23.json .*' 2>&1 | tee jdk-23.txt
 sbt -batch -java-home /usr/lib/jvm/jdk-21 clean 'jsoniter-scala-benchmarkJVM/jmh:run -jvmArgsAppend "-Djmh.executor=FJP" -p size=128 -prof gc -rf json -rff jdk-21.json .*' 2>&1 | tee jdk-21.txt
 sbt -batch -java-home /usr/lib/jvm/jdk-17 clean 'jsoniter-scala-benchmarkJVM/jmh:run -jvmArgsAppend "-Djmh.executor=FJP" -p size=128 -prof gc -rf json -rff jdk-17.json .*' 2>&1 | tee jdk-17.txt
