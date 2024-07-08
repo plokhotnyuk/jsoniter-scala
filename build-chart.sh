443b193fc6839b1254719925bb462538ce7c1be7
plokhotnyuk <plokhotnyuk@gmail.com>
2024-07-08 09:54:03 +0200

    Switch benchmarks to use JDK 24 instead of JDK 23

--- build-chart.sh
+++ build-chart.sh
@@ -1,16 +1,16 @@
 sed -i '/"gc./ s//"·gc./g' *.json
-echo 'var providedBenchmarks = ["JDK 17", "JDK 21", "JDK 23", "GraalVM CE JDK 21", "GraalVM CE JDK 23", "GraalVM JDK 17", "GraalVM JDK 21", "GraalVM JDK 22"];
+echo 'var providedBenchmarks = ["JDK 17", "JDK 21", "JDK 24", "GraalVM CE JDK 21", "GraalVM CE JDK 24", "GraalVM JDK 17", "GraalVM JDK 21", "GraalVM JDK 22"];
 var providedBenchmarkStore = {
 "JDK 17":'
 jq -c . < jdk-17.json
 echo ', "JDK 21":'
 jq -c . < jdk-21.json
-echo ', "JDK 23":'
-jq -c . < jdk-23.json
+echo ', "JDK 24":'
+jq -c . < jdk-24.json
 echo ', "GraalVM CE JDK 21":'
 jq -c . < graalvm-community-jdk-21.json
-echo ', "GraalVM CE JDK 23":'
-jq -c . < graalvm-community-jdk-23.json
+echo ', "GraalVM CE JDK 24":'
+jq -c . < graalvm-community-jdk-24.json
 echo ', "GraalVM JDK 17":'
 jq -c . < graalvm-jdk-17.json
 echo ', "GraalVM JDK 21":'
--- build-t16-chart.sh
+++ build-t16-chart.sh
@@ -1,16 +1,16 @@
 sed -i '/"gc./ s//"·gc./g' *.json
-echo 'var providedBenchmarks = ["JDK 17", "JDK 21", "JDK 23", "GraalVM CE JDK 21", "GraalVM CE JDK 23", "GraalVM JDK 17", "GraalVM JDK 21", "GraalVM JDK 22"];
+echo 'var providedBenchmarks = ["JDK 17", "JDK 21", "JDK 24", "GraalVM CE JDK 21", "GraalVM CE JDK 24", "GraalVM JDK 17", "GraalVM JDK 21", "GraalVM JDK 22"];
 var providedBenchmarkStore = {
 "JDK 17":'
 jq -c . < jdk-17-t16.json
 echo ', "JDK 21":'
 jq -c . < jdk-21-t16.json
-echo ', "JDK 23":'
-jq -c . < jdk-23-t16.json
+echo ', "JDK 24":'
+jq -c . < jdk-24-t16.json
 echo ', "GraalVM CE JDK 21":'
 jq -c . < graalvm-community-jdk-21-t16.json
-echo ', "GraalVM CE JDK 23":'
-jq -c . < graalvm-community-jdk-23-t16.json
+echo ', "GraalVM CE JDK 24":'
+jq -c . < graalvm-community-jdk-24-t16.json
 echo ', "GraalVM JDK 17":'
 jq -c . < graalvm-jdk-17-t16.json
 echo ', "GraalVM JDK 21":'
