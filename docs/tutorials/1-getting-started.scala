1b49510e341afa969d9a108235c45ab6fd4c8159
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2024-11-22 14:57:02 +0100

    Add the Getting Started tutorial

--- docs/tutorials/1-getting-started.scala
+++ docs/tutorials/1-getting-started.scala
@@ -1,41 +1,10 @@
-# Getting started
-Let's start our adventure with jsoniter-scala from parsing and serialization of some complex data structure of nested 
-collections and case classes.
-
-## Prerequisites
-In all tutorials we will use [scala-cli](https://scala-cli.virtuslab.org) for running Scala scripts, so you'll need to install it upfront.
-
-You can use any text editor to copy and paste provided code snippets. Please, also, check Scala CLI Cookbooks 
-documentation if you use [VSCode](https://scala-cli.virtuslab.org/docs/cookbooks/ide/vscode), [Intellij IDEA](https://scala-cli.virtuslab.org/docs/cookbooks/ide/intellij), or [emacs](https://scala-cli.virtuslab.org/docs/cookbooks/ide/emacs) editors.
-
-Support of Scala CLI by IDEs is improving over time, so that [IntelliJ IDEA](https://www.jetbrains.com/idea/) with the latest version of Scala 
-plugin you can try its [improved support for Scala CLI projects](https://blog.jetbrains.com/scala/2024/11/13/intellij-scala-plugin-2024-3-is-out/#scala-cli).
-
-Latest versions of jsoniter-scala libraries require JDK 11 or above. You can use any of its distributions.
-
-In all tutorials we will start from a file with the following dependencies and imports:
-
-```scala
 //> using scala 3
 //> using dep "com.github.plokhotnyuk.jsoniter-scala::jsoniter-scala-core::2.31.3"
 //> using compileOnly.dep "com.github.plokhotnyuk.jsoniter-scala::jsoniter-scala-macros::2.31.3"
 
 import com.github.plokhotnyuk.jsoniter_scala.core._
 import com.github.plokhotnyuk.jsoniter_scala.macros._
-```
 
-Please just copy and paste code snippets from the subsequent steps and run by pressing some `Run` button/keystroke in 
-your IDE or run it using `scala-cli <file_name>` command from the terminal.
-
-Make sure to preserve indentation when copying and pasting because we will use indenting syntax of Scala 3.
-
-## Definition of a complex data structure
-
-Let's imagine that we need to generate a JSON representation of some report for a US shop.
-
-Below is a definition of its data structures and instantiation of some sample data: 
-
-```scala
 enum Category extends Enum[Category]:
   case Electronics, Fashion, HomeGoods
 
@@ -156,52 +125,11 @@ val report = List(
   orderPayment1,
   orderPayment2
 )
-```
 
-## Defining the codec
-
-To derive a codec the report type (`List[OrderPayment]` type in our case) we will use `JsonCodecMaker.make` macros:
-```scala
 given JsonValueCodec[List[OrderPayment]] = JsonCodecMaker.make
-```
 
-An instance of this codec (also known as a type-class instance) is getting to be visible in the scope of subsequent
-calls of parsing and serialization methods. 
-
-## Serialization
-
-Now we are ready to serialize the report. Just need to define some entry point method and call `writeToString`.
-We will also print resulting JSON to the system output to see it as a process output:
-
-```scala
 @main def gettingStarted: Unit =
   val json = writeToString(report)
   println(json)
-```
-
-From this moment you can run the script and see a long JSON string on the screen.
-
-## Parsing
-
-Having the JSON string in memory you can parse it using following lines that should be pasted to the end of the
-`gettingStarted` method:
-```scala
   val parsedReport = readFromString(json)
   println(parsedReport)
-```
-
-Now you can rerun the script and get additionally printed `toString` representation of a report parsed from JSON string.
-
-If something gone wrong you can pick the final version of [a script for this tutorial](1-getting-started.scala) and then run it.
-
-## Challenge
-Experiment with different types of collections and try to find any collection type from the standard Scala library that
-is not supported by `JsonCodecMaker.make` macros to derive codec for serialization *and* parsing.
-
-## Recap
-In this tutorial we learned basics for parsing and serialization of complex nested data structures using `scala-cli`.
-Having definition of data structures and their instances in memory we need to:
-1. Add dependency usage and package imports of `core` and `macros` modules of jsoniter-scala 
-2. Define `given` type-class instance of `JsonValueCodec` for top-level data structure
-3. Call `writeToString` to serialize an instance of complex data structure to JSON representation
-4. Call `readFromString` to parse a complex data structure from JSON representation
