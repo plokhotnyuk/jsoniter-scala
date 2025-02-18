676b46975d195e14eff170ec4b744b0be31143c1
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2025-02-18 09:54:29 +0100

    Update benchmark results for 1 thread

--- build-chart.sh
+++ build-chart.sh
@@ -1,22 +1,22 @@
 sed -i '/"gc./ s//"·gc./g' *.json
-echo 'var providedBenchmarks = ["JDK 17", "JDK 21", "JDK 24", "GraalVM CE JDK 17", "GraalVM CE JDK 21", "GraalVM CE JDK 24", "GraalVM JDK 17", "GraalVM JDK 21", "GraalVM JDK 24"];
+echo 'var providedBenchmarks = ["JDK 17", "JDK 21", "JDK 25", "GraalVM CE JDK 17", "GraalVM CE JDK 21", "GraalVM CE JDK 25", "GraalVM JDK 17", "GraalVM JDK 21", "GraalVM JDK 25"];
 var providedBenchmarkStore = {
 "JDK 17":'
 jq -c . < jdk-17.json
 echo ', "JDK 21":'
 jq -c . < jdk-21.json
-echo ', "JDK 24":'
-jq -c . < jdk-24.json
+echo ', "JDK 25":'
+jq -c . < jdk-25.json
 echo ', "GraalVM CE JDK 17":'
 jq -c . < graalvm-community-jdk-17.json
 echo ', "GraalVM CE JDK 21":'
 jq -c . < graalvm-community-jdk-21.json
-echo ', "GraalVM CE JDK 24":'
-jq -c . < graalvm-community-jdk-24.json
+echo ', "GraalVM CE JDK 25":'
+jq -c . < graalvm-community-jdk-25.json
 echo ', "GraalVM JDK 17":'
 jq -c . < graalvm-jdk-17.json
 echo ', "GraalVM JDK 21":'
 jq -c . < graalvm-jdk-21.json
-echo ', "GraalVM JDK 24":'
-jq -c . < graalvm-jdk-24.json
+echo ', "GraalVM JDK 25":'
+jq -c . < graalvm-jdk-25.json
 echo '}'
