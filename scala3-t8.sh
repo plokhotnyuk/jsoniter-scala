c31f33526f5884a49f00548d380179a03b86e570
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2025-03-19 09:08:30 +0100

    Use compact object headers for JVMs with Java 25-ea

--- scala3-t8.sh
+++ scala3-t8.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sbt -java-home /usr/lib/jvm/jdk-17 ++3.6.4 clean jsoniter-scala-benchmarkJVM/assembly
-/usr/lib/jvm/graalvm-jdk-25/bin/java -jar jsoniter-scala-benchmark/jvm/target/scala-3.6.4/benchmarks.jar -t 8 -p size=512 -prof gc -rf json -rff graalvm-jdk-25-t8.json .* 2>&1 | tee graalvm-jdk-25-t8.txt
+/usr/lib/jvm/graalvm-jdk-25/bin/java -jar jsoniter-scala-benchmark/jvm/target/scala-3.6.4/benchmarks.jar -jvmArgsAppend "-XX:+UseCompactObjectHeaders" -t 8 -p size=512 -prof gc -rf json -rff graalvm-jdk-25-t8.json .* 2>&1 | tee graalvm-jdk-25-t8.txt
+/usr/lib/jvm/jdk-25/bin/java -jar jsoniter-scala-benchmark/jvm/target/scala-3.6.4/benchmarks.jar -jvmArgsAppend "-XX:+UseCompactObjectHeaders" -t 8 -p size=512 -prof gc -rf json -rff jdk-25-t8.json .* 2>&1 | tee jdk-25-t8.txt
+/usr/lib/jvm/graalvm-community-jdk-25/bin/java -jar jsoniter-scala-benchmark/jvm/target/scala-3.6.4/benchmarks.jar -jvmArgsAppend "-XX:+UseCompactObjectHeaders" -t 8 -p size=512 -prof gc -rf json -rff graalvm-community-jdk-25-t8.json .* 2>&1 | tee graalvm-community-jdk-25-t8.txt
 /usr/lib/jvm/graalvm-jdk-21/bin/java -jar jsoniter-scala-benchmark/jvm/target/scala-3.6.4/benchmarks.jar -t 8 -p size=512 -prof gc -rf json -rff graalvm-jdk-21-t8.json .* 2>&1 | tee graalvm-jdk-21-t8.txt
-/usr/lib/jvm/graalvm-jdk-17/bin/java -jar jsoniter-scala-benchmark/jvm/target/scala-3.6.4/benchmarks.jar -t 8 -p size=512 -prof gc -rf json -rff graalvm-jdk-17-t8.json .* 2>&1 | tee graalvm-jdk-17-t8.txt
-/usr/lib/jvm/jdk-25/bin/java -jar jsoniter-scala-benchmark/jvm/target/scala-3.6.4/benchmarks.jar -t 8 -p size=512 -prof gc -rf json -rff jdk-25-t8.json .* 2>&1 | tee jdk-25-t8.txt
 /usr/lib/jvm/jdk-21/bin/java -jar jsoniter-scala-benchmark/jvm/target/scala-3.6.4/benchmarks.jar -t 8 -p size=512 -prof gc -rf json -rff jdk-21-t8.json .* 2>&1 | tee jdk-21-t8.txt
-/usr/lib/jvm/jdk-17/bin/java -jar jsoniter-scala-benchmark/jvm/target/scala-3.6.4/benchmarks.jar -t 8 -p size=512 -prof gc -rf json -rff jdk-17-t8.json .* 2>&1 | tee jdk-17-t8.txt
-/usr/lib/jvm/graalvm-community-jdk-25/bin/java -jar jsoniter-scala-benchmark/jvm/target/scala-3.6.4/benchmarks.jar -t 8 -p size=512 -prof gc -rf json -rff graalvm-community-jdk-25-t8.json .* 2>&1 | tee graalvm-community-jdk-25-t8.txt
 /usr/lib/jvm/graalvm-community-jdk-21/bin/java -jar jsoniter-scala-benchmark/jvm/target/scala-3.6.4/benchmarks.jar -t 8 -p size=512 -prof gc -rf json -rff graalvm-community-jdk-21-t8.json .* 2>&1 | tee graalvm-community-jdk-21-t8.txt
+/usr/lib/jvm/graalvm-jdk-17/bin/java -jar jsoniter-scala-benchmark/jvm/target/scala-3.6.4/benchmarks.jar -t 8 -p size=512 -prof gc -rf json -rff graalvm-jdk-17-t8.json .* 2>&1 | tee graalvm-jdk-17-t8.txt
+/usr/lib/jvm/jdk-17/bin/java -jar jsoniter-scala-benchmark/jvm/target/scala-3.6.4/benchmarks.jar -t 8 -p size=512 -prof gc -rf json -rff jdk-17-t8.json .* 2>&1 | tee jdk-17-t8.txt
 /usr/lib/jvm/graalvm-community-jdk-17/bin/java -jar jsoniter-scala-benchmark/jvm/target/scala-3.6.4/benchmarks.jar -t 8 -p size=512 -prof gc -rf json -rff graalvm-community-jdk-17-t8.json .* 2>&1 | tee graalvm-community-jdk-17-t8.txt
