87f5083f1269d3f59572cf9f6e50336a25d3d9ac
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2025-04-14 09:33:31 +0200

    Move benchmark scripts to the jsoniter-scala-benchmark sub-project

---
+++ jsoniter-scala-benchmark/scala2-t24.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+(cd ..; sbt -java-home /usr/lib/jvm/jdk-17 ++2.13.16 clean jsoniter-scala-benchmarkJVM/assembly)
+/usr/lib/jvm/graalvm-jdk-25/bin/java -jar jvm/target/scala-2.13/benchmarks.jar -jvmArgsAppend "-XX:+UseCompactObjectHeaders" -t 24 -p size=512 -prof gc -rf json -rff graalvm-jdk-25-t24.json .* 2>&1 | tee graalvm-jdk-25-t24.txt
+/usr/lib/jvm/jdk-25/bin/java -jar jvm/target/scala-2.13/benchmarks.jar -jvmArgsAppend "-XX:+UseCompactObjectHeaders" -t 24 -p size=512 -prof gc -rf json -rff jdk-25-t24.json .* 2>&1 | tee jdk-25-t24.txt
+/usr/lib/jvm/graalvm-community-jdk-25/bin/java -jar jvm/target/scala-2.13/benchmarks.jar -jvmArgsAppend "-XX:+UseCompactObjectHeaders" -t 24 -p size=512 -prof gc -rf json -rff graalvm-community-jdk-25-t24.json .* 2>&1 | tee graalvm-community-jdk-25-t24.txt
+/usr/lib/jvm/graalvm-jdk-21/bin/java -jar jvm/target/scala-2.13/benchmarks.jar -t 24 -p size=512 -prof gc -rf json -rff graalvm-jdk-21-t24.json .* 2>&1 | tee graalvm-jdk-21-t24.txt
+/usr/lib/jvm/jdk-21/bin/java -jar jvm/target/scala-2.13/benchmarks.jar -t 24 -p size=512 -prof gc -rf json -rff jdk-21-t24.json .* 2>&1 | tee jdk-21-t24.txt
+/usr/lib/jvm/graalvm-community-jdk-21/bin/java -jar jvm/target/scala-2.13/benchmarks.jar -t 24 -p size=512 -prof gc -rf json -rff graalvm-community-jdk-21-t24.json .* 2>&1 | tee graalvm-community-jdk-21-t24.txt
+/usr/lib/jvm/graalvm-jdk-17/bin/java -jar jvm/target/scala-2.13/benchmarks.jar -t 24 -p size=512 -prof gc -rf json -rff graalvm-jdk-17-t24.json .* 2>&1 | tee graalvm-jdk-17-t24.txt
+/usr/lib/jvm/jdk-17/bin/java -jar jvm/target/scala-2.13/benchmarks.jar -t 24 -p size=512 -prof gc -rf json -rff jdk-17-t24.json .* 2>&1 | tee jdk-17-t24.txt
+/usr/lib/jvm/graalvm-community-jdk-17/bin/java -jar jvm/target/scala-2.13/benchmarks.jar -t 24 -p size=512 -prof gc -rf json -rff graalvm-community-jdk-17-t24.json .* 2>&1 | tee graalvm-community-jdk-17-t24.txt
---
+++ jsoniter-scala-benchmark/scala2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+(cd ..; sbt -java-home /usr/lib/jvm/jdk-17 ++2.13.16 clean jsoniter-scala-benchmarkJVM/assembly)
+/usr/lib/jvm/graalvm-jdk-25/bin/java -jar jvm/target/scala-2.13/benchmarks.jar -jvmArgsAppend "-XX:+UseCompactObjectHeaders" -p size=512 -prof gc -rf json -rff graalvm-jdk-25.json .* 2>&1 | tee graalvm-jdk-25.txt
+/usr/lib/jvm/jdk-25/bin/java -jar jvm/target/scala-2.13/benchmarks.jar -jvmArgsAppend "-XX:+UseCompactObjectHeaders" -p size=512 -prof gc -rf json -rff jdk-25.json .* 2>&1 | tee jdk-25.txt
+/usr/lib/jvm/graalvm-community-jdk-25/bin/java -jar jvm/target/scala-2.13/benchmarks.jar -jvmArgsAppend "-XX:+UseCompactObjectHeaders" -p size=512 -prof gc -rf json -rff graalvm-community-jdk-25.json .* 2>&1 | tee graalvm-community-jdk-25.txt
+/usr/lib/jvm/graalvm-jdk-21/bin/java -jar jvm/target/scala-2.13/benchmarks.jar -p size=512 -prof gc -rf json -rff graalvm-jdk-21.json .* 2>&1 | tee graalvm-jdk-21.txt
+/usr/lib/jvm/jdk-21/bin/java -jar jvm/target/scala-2.13/benchmarks.jar -p size=512 -prof gc -rf json -rff jdk-21.json .* 2>&1 | tee jdk-21.txt
+/usr/lib/jvm/graalvm-community-jdk-21/bin/java -jar jvm/target/scala-2.13/benchmarks.jar -p size=512 -prof gc -rf json -rff graalvm-community-jdk-21.json .* 2>&1 | tee graalvm-community-jdk-21.txt
+/usr/lib/jvm/graalvm-jdk-17/bin/java -jar jvm/target/scala-2.13/benchmarks.jar -p size=512 -prof gc -rf json -rff graalvm-jdk-17.json .* 2>&1 | tee graalvm-jdk-17.txt
+/usr/lib/jvm/jdk-17/bin/java -jar jvm/target/scala-2.13/benchmarks.jar -p size=512 -prof gc -rf json -rff jdk-17.json .* 2>&1 | tee jdk-17.txt
+/usr/lib/jvm/graalvm-community-jdk-17/bin/java -jar jvm/target/scala-2.13/benchmarks.jar -p size=512 -prof gc -rf json -rff graalvm-community-jdk-17.json .* 2>&1 | tee graalvm-community-jdk-17.txt
