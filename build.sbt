e139d081eb87691018e59be4dd9130fbd9203843
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2025-06-05 14:49:36 +0200

    Update Scala 3 to 3.7.1 for jsoniter-scala-benchmark

--- build.sbt
+++ build.sbt
@@ -234,7 +234,7 @@ lazy val `jsoniter-scala-benchmark` = crossProject(JVMPlatform, JSPlatform)
   .settings(commonSettings)
   .settings(noPublishSettings)
   .settings(
-    crossScalaVersions := Seq("3.7.1-RC2", "2.13.16"),
+    crossScalaVersions := Seq("3.7.1", "2.13.16"),
     scalacOptions ++= (CrossVersion.partialVersion(scalaVersion.value) match {
       case Some((2, _)) => Seq("-Wopt", "-opt:l:inline", "-opt-inline-from:**:!java.**")
       case _ => Seq("-source:3.3", "-Xmax-inlines:100")
--- jsoniter-scala-benchmark/scala3-t16.sh
+++ jsoniter-scala-benchmark/scala3-t16.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-(cd ..; sbt -java-home /usr/lib/jvm/jdk-17 ++3.7.1-RC2 clean jsoniter-scala-benchmarkJVM/assembly)
-/usr/lib/jvm/graalvm-jdk-25/bin/java -jar jvm/target/scala-3.7.1-RC2/benchmarks.jar -jvmArgsAppend "-XX:+UseCompactObjectHeaders" -t 16 -p size=512 -prof gc -rf json -rff graalvm-jdk-25-t16.json .* 2>&1 | tee graalvm-jdk-25-t16.txt
-/usr/lib/jvm/jdk-25/bin/java -jar jvm/target/scala-3.7.1-RC2/benchmarks.jar -jvmArgsAppend "-XX:+UseCompactObjectHeaders" -t 16 -p size=512 -prof gc -rf json -rff jdk-25-t16.json .* 2>&1 | tee jdk-25-t16.txt
-/usr/lib/jvm/graalvm-community-jdk-25/bin/java -jar jvm/target/scala-3.7.1-RC2/benchmarks.jar -jvmArgsAppend "-XX:+UseCompactObjectHeaders" -t 16 -p size=512 -prof gc -rf json -rff graalvm-community-jdk-25-t16.json .* 2>&1 | tee graalvm-community-jdk-25-t16.txt
-/usr/lib/jvm/graalvm-jdk-21/bin/java -jar jvm/target/scala-3.7.1-RC2/benchmarks.jar -t 16 -p size=512 -prof gc -rf json -rff graalvm-jdk-21-t16.json .* 2>&1 | tee graalvm-jdk-21-t16.txt
-/usr/lib/jvm/jdk-21/bin/java -jar jvm/target/scala-3.7.1-RC2/benchmarks.jar -t 16 -p size=512 -prof gc -rf json -rff jdk-21-t16.json .* 2>&1 | tee jdk-21-t16.txt
-/usr/lib/jvm/graalvm-community-jdk-21/bin/java -jar jvm/target/scala-3.7.1-RC2/benchmarks.jar -t 16 -p size=512 -prof gc -rf json -rff graalvm-community-jdk-21-t16.json .* 2>&1 | tee graalvm-community-jdk-21-t16.txt
-/usr/lib/jvm/graalvm-jdk-17/bin/java -jar jvm/target/scala-3.7.1-RC2/benchmarks.jar -t 16 -p size=512 -prof gc -rf json -rff graalvm-jdk-17-t16.json .* 2>&1 | tee graalvm-jdk-17-t16.txt
-/usr/lib/jvm/jdk-17/bin/java -jar jvm/target/scala-3.7.1-RC2/benchmarks.jar -t 16 -p size=512 -prof gc -rf json -rff jdk-17-t16.json .* 2>&1 | tee jdk-17-t16.txt
-/usr/lib/jvm/graalvm-community-jdk-17/bin/java -jar jvm/target/scala-3.7.1-RC2/benchmarks.jar -t 16 -p size=512 -prof gc -rf json -rff graalvm-community-jdk-17-t16.json .* 2>&1 | tee graalvm-community-jdk-17-t16.txt
+(cd ..; sbt -java-home /usr/lib/jvm/jdk-17 ++3.7.1 clean jsoniter-scala-benchmarkJVM/assembly)
+/usr/lib/jvm/graalvm-jdk-25/bin/java -jar jvm/target/scala-3.7.1/benchmarks.jar -jvmArgsAppend "-XX:+UseCompactObjectHeaders" -t 16 -p size=512 -prof gc -rf json -rff graalvm-jdk-25-t16.json .* 2>&1 | tee graalvm-jdk-25-t16.txt
+/usr/lib/jvm/jdk-25/bin/java -jar jvm/target/scala-3.7.1/benchmarks.jar -jvmArgsAppend "-XX:+UseCompactObjectHeaders" -t 16 -p size=512 -prof gc -rf json -rff jdk-25-t16.json .* 2>&1 | tee jdk-25-t16.txt
+/usr/lib/jvm/graalvm-community-jdk-25/bin/java -jar jvm/target/scala-3.7.1/benchmarks.jar -jvmArgsAppend "-XX:+UseCompactObjectHeaders" -t 16 -p size=512 -prof gc -rf json -rff graalvm-community-jdk-25-t16.json .* 2>&1 | tee graalvm-community-jdk-25-t16.txt
+/usr/lib/jvm/graalvm-jdk-21/bin/java -jar jvm/target/scala-3.7.1/benchmarks.jar -t 16 -p size=512 -prof gc -rf json -rff graalvm-jdk-21-t16.json .* 2>&1 | tee graalvm-jdk-21-t16.txt
+/usr/lib/jvm/jdk-21/bin/java -jar jvm/target/scala-3.7.1/benchmarks.jar -t 16 -p size=512 -prof gc -rf json -rff jdk-21-t16.json .* 2>&1 | tee jdk-21-t16.txt
+/usr/lib/jvm/graalvm-community-jdk-21/bin/java -jar jvm/target/scala-3.7.1/benchmarks.jar -t 16 -p size=512 -prof gc -rf json -rff graalvm-community-jdk-21-t16.json .* 2>&1 | tee graalvm-community-jdk-21-t16.txt
+/usr/lib/jvm/graalvm-jdk-17/bin/java -jar jvm/target/scala-3.7.1/benchmarks.jar -t 16 -p size=512 -prof gc -rf json -rff graalvm-jdk-17-t16.json .* 2>&1 | tee graalvm-jdk-17-t16.txt
+/usr/lib/jvm/jdk-17/bin/java -jar jvm/target/scala-3.7.1/benchmarks.jar -t 16 -p size=512 -prof gc -rf json -rff jdk-17-t16.json .* 2>&1 | tee jdk-17-t16.txt
+/usr/lib/jvm/graalvm-community-jdk-17/bin/java -jar jvm/target/scala-3.7.1/benchmarks.jar -t 16 -p size=512 -prof gc -rf json -rff graalvm-community-jdk-17-t16.json .* 2>&1 | tee graalvm-community-jdk-17-t16.txt
