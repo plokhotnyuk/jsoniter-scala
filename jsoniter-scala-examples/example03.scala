d77c62d9955add1c3ae3f0e9d96bf579954862e1
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2025-08-04 20:08:26 +0200

    Setting version to 2.37.1

--- jsoniter-scala-examples/example03.scala
+++ jsoniter-scala-examples/example03.scala
@@ -1,4 +1,4 @@
-//> using dep "com.github.plokhotnyuk.jsoniter-scala::jsoniter-scala-core::2.37.0"
+//> using dep "com.github.plokhotnyuk.jsoniter-scala::jsoniter-scala-core::2.37.1"
 
 package com.github.plokhotnyuk.jsoniter_scala.core
 
