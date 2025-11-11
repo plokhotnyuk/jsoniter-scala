a154b39390e060c1cd2bb3e237bc9fe05fd6a7d9
Sarunas Valaskevicius <rakatan@gmail.com>
2025-11-11 10:17:49 +0000

    Fix to remove needless allocations (performance)
    Before this fix, every decode operation would reallocate the `buf`
    Array. This is because at the start of read, pos=head=0.
    
    Then, because we've grown the buffer, at the end of the read operation
    we'd allocate the array AGAIN to shrink it back because it is now larger
    than the preferred buffer size config!
    
    The problem started with the commit:
    
    f0ebbe1e7  Code clean up (Andriy Plokhotnyuk) 2020-01-02|
    
    ```diff
    -      newPos
    -    } else {
    -      if (tail > 0) buf = java.util.Arrays.copyOf(buf, buf.length << 1)
    -      pos
    -    }
    +    } else buf = java.util.Arrays.copyOf(buf, buf.length << 1)
    ```
    
    This change restores the original check for the `tail > 0` which is used
    to indicate that the buffer is currently empty (start of the read
    operation) and we should not grow it.

--- jsoniter-scala-core/js/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/core/JsonReader.scala
+++ jsoniter-scala-core/js/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/core/JsonReader.scala
@@ -4051,7 +4051,9 @@ final class JsonReader private[jsoniter_scala](
       if (mark > 0) mark = 0
       tail = remaining
       head = newPos
-    } else growBuf()
+    } else {
+      if (tail > 0) growBuf()
+    }
     var len = buf.length - tail
     if (bbuf ne null) {
       len = Math.min(bbuf.remaining, len)
